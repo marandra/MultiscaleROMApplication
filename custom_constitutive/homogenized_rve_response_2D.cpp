d8a89b35b8181a2f15b99dce09c7d3674ab6deef
Marcelo <mraschi@cimne.upc.edu>
2016-09-26 12:47:18 +0000

    added constitutive law tamplate for return homogeneized C and sigma

---
+++ custom_constitutive/homogenized_rve_response_2D.cpp
@@ -0,0 +1,319 @@
+#include "homogenized_rve_response_2D.hpp"
+#include "solid_mechanics_application_variables.h"
+
+namespace Kratos
+{
+    // CONSTRUCTOR
+    HomogenizedRVEResponse2D::HomogenizedRVEResponse2D() 
+    	: ConstitutiveLaw()
+    	//, m_initialized(false)
+    	//, m_init_gradT()
+    {
+    }
+    
+    // CLONE
+    ConstitutiveLaw::Pointer HomogenizedRVEResponse2D::Clone() const
+    {
+    	return ConstitutiveLaw::Pointer(new HomogenizedRVEResponse2D());
+    }
+    
+    HomogenizedRVEResponse2D::SizeType HomogenizedRVEResponse2D::WorkingSpaceDimension()
+    {
+    	return 2;
+    }
+    
+    HomogenizedRVEResponse2D::SizeType HomogenizedRVEResponse2D::GetStrainSize()
+    {
+    	return 3;
+    }
+    
+    bool HomogenizedRVEResponse2D::Has(const Variable<double>& rThisVariable)
+    {
+    	return false;
+    }
+    
+    bool HomogenizedRVEResponse2D::Has(const Variable<Vector>& rThisVariable)
+    {
+    	return false;
+    }
+    
+    bool HomogenizedRVEResponse2D::Has(const Variable<Matrix>& rThisVariable)
+    {
+    	return false;
+    }
+    
+    bool HomogenizedRVEResponse2D::Has(const Variable<array_1d<double, 2 > >& rThisVariable)
+    {
+    	return false;
+    }
+    
+    bool HomogenizedRVEResponse2D::Has(const Variable<array_1d<double, 3 > >& rThisVariable)
+    {
+    	return false;
+    }
+    
+    double& HomogenizedRVEResponse2D::GetValue(const Variable<double>& rThisVariable, double& rValue)
+    {
+    	return rValue;
+    }
+    
+    Vector& HomogenizedRVEResponse2D::GetValue(const Variable<Vector>& rThisVariable, Vector& rValue)
+    {
+    	return rValue;
+    }
+    
+    Matrix& HomogenizedRVEResponse2D::GetValue(const Variable<Matrix>& rThisVariable, Matrix& rValue)
+    {
+    	return rValue;
+    }
+    
+    array_1d<double, 2 > & HomogenizedRVEResponse2D::GetValue(const Variable<array_1d<double, 2 > >& rVariable, array_1d<double, 2 > & rValue)
+    {
+    	return rValue;
+    }
+    
+    array_1d<double, 3 > & HomogenizedRVEResponse2D::GetValue(const Variable<array_1d<double, 3 > >& rVariable, array_1d<double, 3 > & rValue)
+    {
+    	return rValue;
+    }
+    
+    void HomogenizedRVEResponse2D::SetValue(const Variable<double>& rVariable,
+    	const double& rValue,
+    	const ProcessInfo& rCurrentProcessInfo)
+    {
+    }
+    
+    void HomogenizedRVEResponse2D::SetValue(const Variable<Vector >& rVariable,
+    	const Vector& rValue, const ProcessInfo& rCurrentProcessInfo)
+    {
+    }
+    
+    void HomogenizedRVEResponse2D::SetValue(const Variable<Matrix >& rVariable,
+    	const Matrix& rValue, const ProcessInfo& rCurrentProcessInfo)
+    {
+    }
+    
+    void HomogenizedRVEResponse2D::SetValue(const Variable<array_1d<double, 2 > >& rVariable,
+    	const array_1d<double, 2 > & rValue,
+    	const ProcessInfo& rCurrentProcessInfo)
+    {
+    }
+    
+    void HomogenizedRVEResponse2D::SetValue(const Variable<array_1d<double, 3 > >& rVariable,
+    	const array_1d<double, 3 > & rValue,
+    	const ProcessInfo& rCurrentProcessInfo)
+    {
+    }
+    
+    bool HomogenizedRVEResponse2D::ValidateInput(const Properties& rMaterialProperties)
+    {
+	return true;
+    }
+    
+    HomogenizedRVEResponse2D::StrainMeasure HomogenizedRVEResponse2D::GetStrainMeasure()
+    {
+    	return ConstitutiveLaw::StrainMeasure_Infinitesimal;
+    }
+    
+    HomogenizedRVEResponse2D::StressMeasure HomogenizedRVEResponse2D::GetStressMeasure()
+    {
+    	return ConstitutiveLaw::StressMeasure_Cauchy;
+    }
+    
+    bool HomogenizedRVEResponse2D::IsIncremental()
+    {
+    	return false;
+    }
+    
+    void HomogenizedRVEResponse2D::InitializeMaterial(
+    	const Properties& material_prop,
+    	const GeometryType& rElementGeometry,
+    	const Vector& rShapeFunctionsValues)
+    {
+	double nu = material_prop[POISSON_RATIO];
+	r_prev = std::sqrt(1 - nu * nu) * material_prop[YIELD_STRESS] / std::sqrt(material_prop[YOUNG_MODULUS]);
+	tau_e = 0.;
+    }
+	    
+    void HomogenizedRVEResponse2D::InitializeSolutionStep(const Properties& rMaterialProperties,
+    	const GeometryType& rElementGeometry,
+    	const Vector& rShapeFunctionsValues,
+    	const ProcessInfo& rCurrentProcessInfo)
+    {
+    }
+    
+    void HomogenizedRVEResponse2D::FinalizeSolutionStep(const Properties& rMaterialProperties,
+    	const GeometryType& rElementGeometry,
+    	const Vector& rShapeFunctionsValues,
+    	const ProcessInfo& rCurrentProcessInfo)
+    {
+        // update of damage threshold
+	r_prev = r;
+    }
+    
+    void HomogenizedRVEResponse2D::InitializeNonLinearIteration(const Properties& rMaterialProperties,
+    	const GeometryType& rElementGeometry,
+    	const Vector& rShapeFunctionsValues,
+    	const ProcessInfo& rCurrentProcessInfo)
+    {
+    }
+    
+    void HomogenizedRVEResponse2D::FinalizeNonLinearIteration(const Properties& rMaterialProperties,
+    	const GeometryType& rElementGeometry,
+    	const Vector& rShapeFunctionsValues,
+    	const ProcessInfo& rCurrentProcessInfo)
+    {
+    }
+    
+    void HomogenizedRVEResponse2D::CalculateMaterialResponsePK1(Parameters& rValues)
+    {
+    //	CalculateMaterialResponseCauchy(rValues);
+    }
+    
+    void HomogenizedRVEResponse2D::CalculateMaterialResponsePK2(Parameters& rValues)
+    {
+    //	CalculateMaterialResponseCauchy(rValues);
+    }
+    
+    void HomogenizedRVEResponse2D::CalculateMaterialResponseKirchhoff(Parameters& rValues)
+    {
+    //	CalculateMaterialResponseCauchy(rValues);
+    }
+    
+    void HomogenizedRVEResponse2D::CalculateMaterialResponseCauchy(Parameters& rValues)
+    {
+    	// get some references
+    	const Properties& matprops = rValues.GetMaterialProperties();
+    	Vector& strain = rValues.GetStrainVector();
+    	Vector& stress = rValues.GetStressVector();
+    	Matrix& constitutiveMatrix = rValues.GetConstitutiveMatrix();
+	double H = matprops[ISOTROPIC_HARDENING_MODULUS];
+	double dpointcoeff;
+	double d, q;
+
+            // sigma_bar = C : epsilon
+    	    CalculateConstitutiveMatrix(matprops, constitutiveMatrix);
+            stress = prod(constitutiveMatrix, strain);
+            // tau_epsilon = sqrt(epsilon : sigma_bar)
+            tau_e = std::sqrt(inner_prod(strain, stress));
+
+	    // r = r_prev
+	    // d = 1 - q(r) / r
+	    // sigma = (1 - d) * sigma_bar
+	    // C_tan = (1 - d) * C
+            if (tau_e <= r_prev) {
+                r = r_prev;
+                q = CalculateQ(r, matprops);
+                d = 1. - q / r;
+                stress *= (1 - d); 
+                constitutiveMatrix *= (1 - d); 
+	    }
+	    // r = tau_e
+	    // d = 1 - q(r) / r
+	    // sigma = (1 - d) * sigma_bar
+	    // C_tan = (1 - d) * C - q(r)-H/r3 * sigma x sigma
+	    else {
+                r = tau_e;
+                q = CalculateQ(r, matprops);
+                d = 1. - q / r;
+                stress *= (1. - d); 
+                dpointcoeff = (q - H * r)/(r * r * r);
+                constitutiveMatrix *= (1. - d); 
+                constitutiveMatrix -= dpointcoeff * outer_prod(stress, stress); 
+	    }
+
+	    //std::cout << "DEBUG strain " << strain<< std::endl;
+	    //std::cout << "DEBUG stress " << stress<< std::endl;
+	    //std::cout << "DEBUG tau            " << tau_e << std::endl;
+	    //std::cout << "DEBUG r " << r_prev << std::endl;
+	    //std::cout << "DEBUG C_sec " << constitutiveMatrix << std::endl;
+	    //std::cout << "DEBUG curve " << tau_e << " " << r << " " << q << " " << std::endl;
+
+    }
+    
+    void HomogenizedRVEResponse2D::FinalizeMaterialResponsePK1(Parameters& rValues)
+    {
+    //	FinalizeMaterialResponseCauchy(rValues);
+    }
+    
+    void HomogenizedRVEResponse2D::FinalizeMaterialResponsePK2(Parameters& rValues)
+    {
+    //	FinalizeMaterialResponseCauchy(rValues);
+    }
+    
+    void HomogenizedRVEResponse2D::FinalizeMaterialResponseKirchhoff(Parameters& rValues)
+    {
+    //	FinalizeMaterialResponseCauchy(rValues);
+    }
+    
+    void HomogenizedRVEResponse2D::FinalizeMaterialResponseCauchy(Parameters& rValues)
+    {
+    }
+    
+    void HomogenizedRVEResponse2D::ResetMaterial(const Properties& rMaterialProperties,
+    	const GeometryType& rElementGeometry,
+    	const Vector& rShapeFunctionsValues)
+    {
+    }
+    
+    double HomogenizedRVEResponse2D::CalculateQ(double r,
+    	const Properties& material_prop) {
+
+	double H = material_prop[ISOTROPIC_HARDENING_MODULUS];
+	double nu = material_prop[POISSON_RATIO];
+	double r0 = std::sqrt(1 - nu * nu) * material_prop[YIELD_STRESS] / std::sqrt(material_prop[YOUNG_MODULUS]);
+    	double q_inf = std::sqrt(1 - nu * nu) * material_prop[INFINITY_YIELD_STRESS] / std::sqrt(material_prop[YOUNG_MODULUS]);
+        double q;
+
+	if (r < r0)
+	    return r;
+	q = q_inf - (q_inf - r0) * std::exp(H * (1 - r / r0));
+	return q;
+    }
+
+    void HomogenizedRVEResponse2D::CalculateConstitutiveMatrix(
+        const Properties& props, Matrix& D)
+    {
+    	double E = props[YOUNG_MODULUS];
+    	double nu = props[POISSON_RATIO];
+	double Ebar = E / (1. - nu * nu);
+	double nubar = nu / (1. - nu);
+
+    	D.clear();
+    
+        D(0, 0) = 1;     D(0, 1) = nubar; D(0, 2) = 0; 
+        D(1, 0) = nubar; D(1, 1) = 1;     D(1, 2) = 0; 
+        D(2, 0) = 0;     D(2, 1) = 0;     D(2, 2) = 0.5 * (1 - nubar); 
+
+	D *= Ebar / (1. - nubar * nubar);
+    }
+    
+    void HomogenizedRVEResponse2D::GetLawFeatures(Features& rFeatures)
+    {
+    	rFeatures.mOptions.Set(PLANE_STRAIN_LAW);
+    	rFeatures.mOptions.Set(INFINITESIMAL_STRAINS);
+    	rFeatures.mOptions.Set(ISOTROPIC);
+    	rFeatures.mStrainMeasures.push_back(StrainMeasure_Infinitesimal);
+    	rFeatures.mStrainSize = GetStrainSize();
+    	rFeatures.mSpaceDimension = WorkingSpaceDimension();
+    }
+    
+    int HomogenizedRVEResponse2D::Check(
+    	const Properties& rMaterialProperties,
+    	const GeometryType& rElementGeometry,
+    	const ProcessInfo& rCurrentProcessInfo)
+    {
+    		if(!rMaterialProperties.Has(YOUNG_MODULUS)) 
+    		    KRATOS_THROW_ERROR(std::invalid_argument, "HomogenizedRVEResponse2D - missing YOUNG_MODULUS", "");
+    		if(!rMaterialProperties.Has(POISSON_RATIO)) 
+    		    KRATOS_THROW_ERROR(std::invalid_argument, "HomogenizedRVEResponse2D - missing POISSON_RATIO", "");
+    		if(!rMaterialProperties.Has(ISOTROPIC_HARDENING_MODULUS)) 
+    		KRATOS_THROW_ERROR(std::invalid_argument, "HomogenizedRVEResponse2D - missing ISOTROPIC_HARDENING_MODULUS", "");
+    		if(!rMaterialProperties.Has(INFINITY_YIELD_STRESS)) 
+    		KRATOS_THROW_ERROR(std::invalid_argument, "HomogenizedRVEResponse2D - missing INFINITY_YIELD_STRESS", "");
+    		if(rMaterialProperties[INFINITY_YIELD_STRESS] < 0) 
+    		    KRATOS_THROW_ERROR(std::invalid_argument, "HomogenizedRVEResponse2D - INFINITY_YIELD_STRESS must be positive", "");
+    		return 0;
+    }
+    
+} /* namespace Kratos.*/
