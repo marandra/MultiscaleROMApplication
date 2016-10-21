cf0594944d50d995887966e89c813a8553ba9beb
Marcelo <mraschi@cimne.upc.edu>
2016-10-21 12:53:30 +0000

    Partial port of solid mechanic solver to rve adpater

--- python_scripts/solve_RVE.py
+++ python_scripts/solve_RVE.py
@@ -115,29 +115,27 @@ class SolveRVE(km.Process):
         for i in range(params["solver_settings"]["processes_sub_model_part_list"].size()):
             part_name = params["solver_settings"]["processes_sub_model_part_list"][i].GetString()
             self.model.update({part_name: self.model_part.GetSubModelPart(part_name)})
-
         self.processes = process_factory.KratosProcessFactory(self.model)\
             .ConstructListOfProcesses(params["constraints_process_list"])
         self.processes += process_factory.KratosProcessFactory(self.model)\
             .ConstructListOfProcesses(params["loads_process_list"])
 
-
         f = operator.attrgetter(params['problem_data']['strain_size'].GetString())
         self.StrainSize = f(self.RVEStrainSize)
         if(self.StrainSize == self.RVEStrainSize.RVE_THERMAL_PLANE_STRESS):
-        	self.RveAdapterClass = mss.RveThermal2DAdapterV2
-        	self.RveMaterialClass = mss.RveConstitutiveLawV2Thermal2D
+                self.RveAdapterClass = mss.RveThermal2DAdapterV2
+                self.RveMaterialClass = mss.RveConstitutiveLawV2Thermal2D
         elif(self.StrainSize == self.RVEStrainSize.RVE_THERMAL_3D):
-        	self.RveAdapterClass = mss.RveThermal3DAdapterV2
-        	self.RveMaterialClass = mss.RveConstitutiveLawV2Thermal3D
+                self.RveAdapterClass = mss.RveThermal3DAdapterV2
+                self.RveMaterialClass = mss.RveConstitutiveLawV2Thermal3D
         elif(self.StrainSize == self.RVEStrainSize.RVE_PLANE_STRESS):
-        	self.RveAdapterClass = mss.RvePlaneStressAdapterV2
-        	self.RveMaterialClass = mss.RveConstitutiveLawV2PlaneStress
+                self.RveAdapterClass = mss.RvePlaneStressAdapterV2
+                self.RveMaterialClass = mss.RveConstitutiveLawV2PlaneStress
         elif(self.StrainSize == self.RVEStrainSize.RVE_PLANE_STRAIN):
-        	raise Exception("Rve Plane Strain Not Yet Implemented")
+                raise Exception("Rve Plane Strain Not Yet Implemented")
         else: # RVEStrainSize.RVE_3D
-        	self.RveAdapterClass = mss.Rve3DAdapterV2
-        	self.RveMaterialClass = mss.RveConstitutiveLawV23D
+                self.RveAdapterClass = mss.Rve3DAdapterV2
+                self.RveMaterialClass = mss.RveConstitutiveLawV23D
         self.BoundingPolygonNodesID = None
         self.RveGeometryDescr = None
         self.ResultsIOClass = TK_GiD.ResultsIO
@@ -145,24 +143,10 @@ class SolveRVE(km.Process):
         self.ResultsOnGaussPoints = [km.GREEN_LAGRANGE_STRAIN_TENSOR, km.CAUCHY_STRESS_TENSOR]
         self.RveConstraintHandlerClass = mss.RveConstraintHandler_PBF_SD
         self.RveHomogenizerClass       = mss.RveHomogenizer
-        #self.SchemeClass               = mss.RveStaticScheme
-
-        #self.LinearSolverClass = mss.SkylineLUFactorizationSolverV2
-        #self.LinearSolverClass = km.SkylineLUFactorizationSolver
-        #self.LinearSolverClass = self.solver.linear_solver
-        
-        #self.MaxIterations = 10
-        #self.CalculateReactions = True
-        #self.ReformDofSetAtEachIteration = False
-        #self.MoveMesh = False
-        #self.ConvergenceCriteriaClass = mss.ResidualNormCriteria
-        #self.ConvergenceRelativeTolerance = 1.0E-7
-        #self.ConvergenceAbsoluteTolerance = 1.0E-9
-        #self.ConvergenceIsVerbose = False
         self.OutputElementList = [1]
         self.TrackList = {}
         self.Initialized = False
-	
+        
  
     def ExecuteInitialize(self):
         self.Initialize()
@@ -176,7 +160,7 @@ class SolveRVE(km.Process):
         pass
 
     def ExecuteAfterOutputStep(self):
-#	self.__write_output(self.model_part.ProcessInfo[TIME])
+#       self.__write_output(self.model_part.ProcessInfo[TIME])
         pass
 
     def ExecuteBeforeOutputStep(self):
@@ -186,160 +170,164 @@ class SolveRVE(km.Process):
         pass
 
     def ExecuteFinalizeSolutionStep(self):
-    	#t = self.Model.ProcessInfo[km.TIME]
-    	#if t == self.Model.ProcessInfo[km.END_TIME] or self.__check_write_freq(t):
+        #t = self.Model.ProcessInfo[km.TIME]
+        #if t == self.Model.ProcessInfo[km.END_TIME] or self.__check_write_freq(t):
 #        self.write_results() 
         pass
     
     def ExecuteFinalize(self):
-#	if(self.Initialized == True):
-#	    self.__finalize_output()
+#       if(self.Initialized == True):
+#           self.__finalize_output()
         pass
-				
+                                
    
     def Initialize(self):
 
-	def __generate_rve_constitutive_law():
-	# This method generates a new rve constitutive law
-	# by cloning the rve model part prototype and creating
-	# a new rve constitutive law out of it.
-	# This method is meant to be private, do NOT call it explicitly
-	
-		modelPartClone = km.ModelPart(self.model_part_name + "_RVE")
-		mss.RveCloneModelPart(self.model_part, modelPartClone) # clone the model part prototype
-		
-		msData = mss.RveMacroscaleData() 
-		
-		constraint_handler = self.RveConstraintHandlerClass()
-		
-		homogenizer = self.RveHomogenizerClass()
-		
-		adapter = self.RveAdapterClass() # generate the rve adapter
-		
+        def __generate_rve_constitutive_law():
+        # This method generates a new rve constitutive law
+        # by cloning the rve model part prototype and creating
+        # a new rve constitutive law out of it.
+        # This method is meant to be private, do NOT call it explicitly
+
+        
+            def port_solver_to_adapter_args():
+                scheme = self.solver._GetSolutionScheme(
+                        self.solver.settings["analysis_type"].GetString(), 
+                        self.solver.settings["component_wise"].GetBool(),
+                        self.solver.settings["compute_contact_forces"].GetBool())
+                convergence = self.solver._GetConvergenceCriterion()
+                linear_solver = self.solver.linear_solver
                 print("DEBUGG")
                 print(self.solver._GetBuilderAndSolver(False, False))
+                return scheme, convergence, linear_solver 
 
-		adapter.SetRveData(
-			modelPartClone,
-			msData,
-			self.RveGeometryDescr,
-			constraint_handler,
-			mss.RveLinearSystemOfEquations(self.solver.linear_solver),
-                        #self.solver._GetBuilderAndSolver(False, False),
-			homogenizer,
-			self.solver._GetSolutionScheme("Non-Linear", False, False), 
-			#self.solver.mpScheme, 
-                        self.solver._GetConvergenceCriterion()
-		        )
-		
-		rveLaw = self.RveMaterialClass(adapter) # finally generate the constitutive law adapter
-		
-		for i in range(modelPartClone.GetBufferSize()):
-		    modelPartClone.CloneTimeStep(0.0)
-		return  (rveLaw,modelPartClone) # return a tuple
-	
 
-	def __track_rve_constitutive_law(rveLaw, elemID, gpID):
-	# This method tracks a rve constitutive law
-	# at a given element in a given gauss point.
-	# This method is meant to be private, do NOT call it explicitly
-		elInfo = TK_Rve_V2.SolidElementInfo(elemID, gpID)
-		
-		if( next((x for x in self.OutputElementList if x == elemID), None) is not None ):
-			outputFileName = self.model_part.Name + "__" + elInfo.GetStringExtension()
-			rveLawIO = self.ResultsIOClass(rveLaw.GetModelPart(), outputFileName, self.ResultsOnNodes, self.ResultsOnGaussPoints)
-			self.TrackList[elInfo] = (rveLaw, rveLawIO)
-		else:
-			self.TrackList[elInfo] = (rveLaw, None)
-	
-	def __assign_rve_constitutive_law(Element):
-	# This method assignes a rve constitutive law
-	# at a given element.
-	# This method is meant to be private, do NOT call it explicitly
-		
-		# list of generated rve mdpa clones
-		rve_mdpa_clones = []
-		
-		# get the number of integration points
-		elemIntPoints = Element.GetIntegrationPoints()
-		num_gp = len(elemIntPoints)
-		elem_id = Element.Id
-		
-		# get a reference to the process into
-		pinfo = self.model_part.ProcessInfo
-		
-		# prepare the list of constitutive laws for the element
-		constitutiveLaws = []
-		
-		# for each element integration point ...
-		for gp_id in range(num_gp):
-			
-			# generate a new rve constitutive law
-			rve_law__rve_mdpa__tuple = __generate_rve_constitutive_law()
-			aRveLaw = rve_law__rve_mdpa__tuple[0]
-			constitutiveLaws.append(aRveLaw)
-			
-			# TODO: check what rve law to track...
-			# for the moment let's track them all
-			__track_rve_constitutive_law(aRveLaw, elem_id, gp_id)
-			
-			# store the rve mdpa clone
-			rve_mdpa_clones.append(rve_law__rve_mdpa__tuple[1])
-		
-		# assign the list of constitutive laws
-		Element.SetValuesOnIntegrationPoints(sol.CONSTITUTIVE_LAW_POINTER, constitutiveLaws, pinfo)
-		
-		return rve_mdpa_clones
-	
+            modelPartClone = km.ModelPart(self.model_part_name)
+            mss.RveCloneModelPart(self.model_part, modelPartClone)
+            constraint_handler = self.RveConstraintHandlerClass()
+            homogenizer = self.RveHomogenizerClass()
+            scheme, convergence, linear_solver = port_solver_to_adapter_args()
 
-	def __initialize_output():
-	## Initializes the output for the tracked rves (only if required)
-		for key, value in self.TrackList.items():
-			rveIO = value[1]
-			if(rveIO is not None):
-				rveIO.Initialize()
-	
+            adapter = self.RveAdapterClass() 
+            adapter.SetRveData(
+                modelPartClone,
+                mss.RveMacroscaleData(),
+                self.RveGeometryDescr,
+                constraint_handler,
+                mss.RveLinearSystemOfEquations(linear_solver),
+                homogenizer,
+                scheme,
+                convergence
+                )
+        
+            rveLaw = self.RveMaterialClass(adapter) # finally generate the constitutive law adapter
+            
+            for i in range(modelPartClone.GetBufferSize()):
+                modelPartClone.CloneTimeStep(0.0)
+            return  (rveLaw,modelPartClone) # return a tuple
+    
 
-	def __write_output(self, currentTime):
-	## Writes the output for the tracked rves (only if required)
-		for key, value in self.TrackList.items():
-			rveIO = value[1]
-			if(rveIO is not None):
-				rveIO.Write(currentTime)
-	
+        def __track_rve_constitutive_law(rveLaw, elemID, gpID):
+        # This method tracks a rve constitutive law
+        # at a given element in a given gauss point.
+        # This method is meant to be private, do NOT call it explicitly
+                elInfo = TK_Rve_V2.SolidElementInfo(elemID, gpID)
+                
+                if( next((x for x in self.OutputElementList if x == elemID), None) is not None ):
+                        outputFileName = self.model_part.Name + "__" + elInfo.GetStringExtension()
+                        rveLawIO = self.ResultsIOClass(rveLaw.GetModelPart(), outputFileName, self.ResultsOnNodes, self.ResultsOnGaussPoints)
+                        self.TrackList[elInfo] = (rveLaw, rveLawIO)
+                else:
+                        self.TrackList[elInfo] = (rveLaw, None)
+        
+        def __assign_rve_constitutive_law(Element):
+        # This method assignes a rve constitutive law
+        # at a given element.
+        # This method is meant to be private, do NOT call it explicitly
+                
+                # list of generated rve mdpa clones
+                rve_mdpa_clones = []
+                
+                # get the number of integration points
+                elemIntPoints = Element.GetIntegrationPoints()
+                num_gp = len(elemIntPoints)
+                elem_id = Element.Id
+                
+                # get a reference to the process into
+                pinfo = self.model_part.ProcessInfo
+                
+                # prepare the list of constitutive laws for the element
+                constitutiveLaws = []
+                
+                # for each element integration point ...
+                for gp_id in range(num_gp):
+                        
+                        # generate a new rve constitutive law
+                        rve_law__rve_mdpa__tuple = __generate_rve_constitutive_law()
+                        aRveLaw = rve_law__rve_mdpa__tuple[0]
+                        constitutiveLaws.append(aRveLaw)
+                        
+                        # TODO: check what rve law to track...
+                        # for the moment let's track them all
+                        __track_rve_constitutive_law(aRveLaw, elem_id, gp_id)
+                        
+                        # store the rve mdpa clone
+                        rve_mdpa_clones.append(rve_law__rve_mdpa__tuple[1])
+                
+                # assign the list of constitutive laws
+                Element.SetValuesOnIntegrationPoints(sol.CONSTITUTIVE_LAW_POINTER, constitutiveLaws, pinfo)
+                
+                return rve_mdpa_clones
+        
 
-	def __finalize_output(self):
-	## Finalizes the output for the tracked rves (only if required)
-		for key, value in self.TrackList.items():
-			rveIO = value[1]
-			if(rveIO is not None):
-				rveIO.Finalize()
-	
+        def __initialize_output():
+        ## Initializes the output for the tracked rves (only if required)
+                for key, value in self.TrackList.items():
+                        rveIO = value[1]
+                        if(rveIO is not None):
+                                rveIO.Initialize()
+        
 
-	def __print_info(self):
-		
-		print ("")
-		print ("====================================================")
-		print ("RveModelerShell - Info:")
-		print ("====================================================")
-		print ("MODEL PART - PROTOTYPE:")
-		print (self.model_part)
-		print ("====================================================")
-		print ("TRACK LIST:")
-		ii = 0
-		print ("+--------------------------------------------------------+")
-		for key, value in self.TrackList.items():
-			print ("AT[", ii, "]")
-			print ("Info:")
-			print (key)
-			print ("(RveMaterial, IO)")
-			print (value)
-			print ("Micro Model Clone:")
-			micro = value[0].GetModelPart()
-			print (hex(id(micro)))
-			print (micro)
-			print ("+--------------------------------------------------------+")
-			ii+=1
+        def __write_output(self, currentTime):
+        ## Writes the output for the tracked rves (only if required)
+                for key, value in self.TrackList.items():
+                        rveIO = value[1]
+                        if(rveIO is not None):
+                                rveIO.Write(currentTime)
+        
+
+        def __finalize_output(self):
+        ## Finalizes the output for the tracked rves (only if required)
+                for key, value in self.TrackList.items():
+                        rveIO = value[1]
+                        if(rveIO is not None):
+                                rveIO.Finalize()
+        
+
+        def __print_info(self):
+                
+                print ("")
+                print ("====================================================")
+                print ("RveModelerShell - Info:")
+                print ("====================================================")
+                print ("MODEL PART - PROTOTYPE:")
+                print (self.model_part)
+                print ("====================================================")
+                print ("TRACK LIST:")
+                ii = 0
+                print ("+--------------------------------------------------------+")
+                for key, value in self.TrackList.items():
+                        print ("AT[", ii, "]")
+                        print ("Info:")
+                        print (key)
+                        print ("(RveMaterial, IO)")
+                        print (value)
+                        print ("Micro Model Clone:")
+                        micro = value[0].GetModelPart()
+                        print (hex(id(micro)))
+                        print (micro)
+                        print ("+--------------------------------------------------------+")
+                        ii+=1
 
         if(self.Initialized == False):
             # initialize the geometry descriptor
