767e96717a9be2f21c5fc57de09aa69c5c37e0e6
Marcelo Raschi <mraschi@cimne.upc.edu>
2017-01-27 14:09:11 +0100

    formatting

--- python_scripts/write_element_variables.py
+++ python_scripts/write_element_variables.py
@@ -1,7 +1,8 @@
 import KratosMultiphysics as km
-import KratosMultiphysics.MultiScaleApplication as mss # <- check is used
+import KratosMultiphysics.MultiScaleApplication as mss  # <- check is used
 import os
 import operator
+
 
 def Factory(settings, Model):
     return WriteElementVariables(settings["Parameters"], Model)
@@ -17,27 +18,30 @@ def parameters_get_list_int(ilist):
 class WriteElementVariables(km.Process):
     def __init__(self, param, Model):
         self.model_part = Model[param['model_part_name'].GetString()]
-    	self.filename = param['filename'].GetString()
+        self.filename = param['filename'].GetString()
         self.vname1 = param['variable_1'].GetString()
         f = operator.attrgetter(self.vname1)
         self.Var1 = f(km)
         self.vname2 = param['variable_2'].GetString()
         f = operator.attrgetter(self.vname2)
         self.Var2 = f(km)
-    	self.elem = self.model_part.Elements[param['element'].GetInt()]
-    	self.gp = param['gauss_point'].GetInt()
-    
+        self.elem = self.model_part.Elements[param['element'].GetInt()]
+        self.gp = param['gauss_point'].GetInt()
+
     def write_results(self):
-    	with open(self.filename, 'a') as ofile:
+        with open(self.filename, 'a') as ofile:
             process_info = self.model_part.ProcessInfo
             CX = self.elem.GetValuesOnIntegrationPoints(self.Var1, process_info)[self.gp]
             CY = self.elem.GetValuesOnIntegrationPoints(self.Var2, process_info)[self.gp]
-            ofile.write("{: .3e} {: .3e} {: .3e}  {: .3e} {: .3e} {: .3e}" #"  {: .3e} {: .3e} {: .3e} {: .3e}"
-                .format(
-                CX[0], CX[3], CX[1], # CX[0],
-                CY[0], CY[3], CY[1], # CY[0],
-                ))
-            ofile.write("\n")
+            print("DEBUG")
+            print(CX)
+            print(CY)
+            #ofile.write("{: .3e} {: .3e} {: .3e}  {: .3e} {: .3e} {: .3e}"  #"  {: .3e} {: .3e} {: .3e} {: .3e}"
+            #    .format(
+            #        CX[0], CX[3], CX[1],  # CX[0],
+            #        CY[0], CY[3], CY[1],  # CY[0],
+            #        ))
+            #ofile.write("\n")
     
     def ExecuteInitialize(self):
         try:
@@ -56,8 +60,8 @@ class WriteElementVariables(km.Process):
                                                                             
 
         #self.write_results() 
-    	#self.Tn = self.Model.ProcessInfo[km.TIME]
-    
+        #self.Tn = self.Model.ProcessInfo[km.TIME]
+
     def ExecuteInitializeSolutionStep(self):
         pass
 
@@ -71,9 +75,10 @@ class WriteElementVariables(km.Process):
         pass
 
     def ExecuteFinalizeSolutionStep(self):
-    	#t = self.Model.ProcessInfo[km.TIME]
-    	#if t == self.Model.ProcessInfo[km.END_TIME] or self.__check_write_freq(t):
-        self.write_results() 
-    
+        #t = self.Model.ProcessInfo[km.TIME]
+        #if t == self.Model.ProcessInfo[km.END_TIME] or self.__check_write_freq(t):
+        self.write_results()
+
+
     def ExecuteFinalize(self):
         pass
