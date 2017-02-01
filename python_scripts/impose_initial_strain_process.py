1078170fb82a12609ab52c65d11f22c50c43382d
Marcelo Raschi <mraschi@cimne.upc.edu>
2017-02-01 18:12:35 +0100

    added process to impose initial strain

---
+++ python_scripts/impose_initial_strain_process.py
@@ -0,0 +1,84 @@
+import KratosMultiphysics as km
+import bisect
+import os
+
+
+def Factory(settings, Model):
+    if(type(settings) != km.Parameters):
+        raise Exception("expected input is Parameters object, encapsulating a json string")
+    return ImposeInitialStrainProcess(Model, settings["Parameters"])
+
+
+def parameters_get_list_doubles(settings_list):
+    olist = []
+    for i in range(settings_list.size()):
+        olist.append(settings_list[i].GetDouble())
+    return olist
+
+
+def get_multiplier(self):
+    time = self.model_part.ProcessInfo[km.TIME]
+    # interpolator is only valid within range defined in lookuptable
+    # for times outside range, we force time to be the corresponding extreme.
+    if(time <= self.lookuptable['time'][0]):
+        time = self.lookuptable['time'][0]
+    if(time >= self.lookuptable['time'][-1]):
+        time = self.lookuptable['time'][-1]
+    return self.time_interpolator[time]
+
+
+class Interpolate(object):
+  
+    def __init__(self, x_list, y_list):
+        if any([y - x <= 0 for x, y in zip(x_list, x_list[1:])]):
+            raise ValueError("x_list must be in strictly ascending order!")
+        x_list = self.x_list = list(map(float, x_list))
+        y_list = self.y_list = list(map(float, y_list))
+        intervals = zip(x_list, x_list[1:], y_list, y_list[1:])
+        self.slopes = [(y2 - y1)/(x2 - x1) for x1, x2, y1, y2 in intervals]
+
+    def __getitem__(self, x):
+        i = bisect.bisect(self.x_list, x) - 1
+        if i >= len(self.slopes):
+            i = len(self.slopes) - 1
+        return self.y_list[i] + self.slopes[i] * (x - self.x_list[i])
+
+        
+class ImposeInitialStrainProcess(km.Process):
+
+    def __init__(self, Model, settings):
+        km.Process.__init__(self)
+        self.model_part = Model[settings["model_part_name"].GetString()]
+        self.process_info = self.model_part.ProcessInfo
+        self.lookuptable = {
+            'time': parameters_get_list_doubles(settings["lookuptable_time"]),
+            'mult': parameters_get_list_doubles(settings["lookuptable_mult"])}
+        self.time_interpolator = Interpolate(
+            self.lookuptable['time'], self.lookuptable['mult'])
+        init_strain_list =  parameters_get_list_doubles(settings["initial_strain"])
+        self.nr_gp_elem = len(
+            self.model_part.Elements[1].GetValuesOnIntegrationPoints(
+                km.INITIAL_STRAIN, self.process_info))
+        self.len_strain = len(init_strain_list)
+        self.initial_strain = km.Vector(self.len_strain)
+        for i, s in enumerate(init_strain_list):
+            self.initial_strain[i] = s
+
+    def ExecuteInitialize(self):
+        pass
+
+    def ExecuteInitializeSolutionStep(self):
+        scaling_factor = get_multiplier(self)
+        # TODO pasar opcion por variable de proceso?
+        strain = scaling_factor * self.initial_strain
+        strain_array = self.nr_gp_elem * [strain]
+        for elem in self.model_part.Elements:
+            elem.SetValuesOnIntegrationPoints(
+                km.INITIAL_STRAIN, strain_array, self.len_strain, self.process_info)
+
+    def ExecuteFinalizeSolutionStep(self):
+        pass
+
+    def ExecuteFinalize(self):
+        pass
+
