2d4dfa6193d50e15eb905bb7b0c16150d05035ae
Marcelo Raschi <mraschi@cimne.upc.edu>
2017-03-27 18:08:05 +0200

    Format

--- multiscale_rom_application.cpp
+++ multiscale_rom_application.cpp
@@ -1,57 +1,84 @@
-#include "includes/variables.h"
+#include "multiscale_rom_application.h"
+#include "geometries/hexahedra_3d_8.h"
 #include "geometries/line_2d_3.h"
 #include "geometries/line_3d_3.h"
 #include "geometries/quadrilateral_2d_4.h"
-#include "geometries/hexahedra_3d_8.h"
-#include "multiscale_rom_application.h"
+#include "includes/variables.h"
 namespace Kratos {
-    //Application variables creation: (see solid_mechanics_application_variables.cpp)
-    //Application Constructor:
-    KratosMultiscaleROMApplication::KratosMultiscaleROMApplication():
-        mSmallDisplacementBbarElement2D4N( 0, Element::GeometryType::Pointer( new Quadrilateral2D4 <Node<3> >( Element::GeometryType::PointsArrayType( 4 ) ) ) ),
-        mSmallDisplacementHpromElement2D4N( 0, Element::GeometryType::Pointer( new Quadrilateral2D4 <Node<3> >( Element::GeometryType::PointsArrayType( 4 ) ) ) ),
-        mSmallDisplacementBbarElement3D8N( 0, Element::GeometryType::Pointer( new Hexahedra3D8 <Node<3> >( Element::GeometryType::PointsArrayType( 8 ) ) ) ),
-        mSmallDisplacementHpromElement3D8N( 0, Element::GeometryType::Pointer( new Hexahedra3D8 <Node<3> >( Element::GeometryType::PointsArrayType( 8 ) ) ) ),
-        mUpdatedLagrangianFbarElement2D4N( 0, Element::GeometryType::Pointer( new Quadrilateral2D4 <Node<3> >( Element::GeometryType::PointsArrayType( 4 ) ) ) ),
-        mTotalLagrangianFbarElement2D4N( 0, Element::GeometryType::Pointer( new Quadrilateral2D4 <Node<3> >( Element::GeometryType::PointsArrayType( 4 ) ) ) ),
-        mMinimalKineticCondition2D3N( 0, Condition::GeometryType::Pointer( new Line2D3 <Node<3> >( Condition::GeometryType::PointsArrayType( 3 ) ) ) ),
-        mMinimalKineticCondition3D3N( 0, Condition::GeometryType::Pointer( new Line3D3 <Node<3> >( Condition::GeometryType::PointsArrayType( 3 ) ) ) )
-    {}
-    
-    void KratosMultiscaleROMApplication::Register() {
-        // calling base class register to register Kratos components
-        KratosApplication::Register();
-        std::cout << "Initializing KratosMultiscaleROMApplication...  " << std::endl;
-        
-        //Register Variables (variables created in solid_mechanics_application_variables.cpp)
-        KRATOS_REGISTER_VARIABLE(ISOTROPIC_DAMAGE_MODULUS)
-        KRATOS_REGISTER_VARIABLE(INFINITY_YIELD_STRESS)
-        KRATOS_REGISTER_VARIABLE(FLOW_RULE_IS_TRACTION_ONLY)
-        KRATOS_REGISTER_VARIABLE(LAGRANGE_MULTIPLIER_1)
-        KRATOS_REGISTER_VARIABLE(LAGRANGE_MULTIPLIER_2)
-        KRATOS_REGISTER_VARIABLE(LAGRANGE_MULTIPLIER_3)
-        KRATOS_REGISTER_VARIABLE(INITIAL_STRAIN_VECTOR)
-        KRATOS_REGISTER_VARIABLE(NUMBER_REDUCED_MODES)
-        KRATOS_REGISTER_VARIABLE(REDUCED_MODES_MATRIX)
-        KRATOS_REGISTER_VARIABLE(REDUCED_MODES_WEIGHTS)
-        KRATOS_REGISTER_VARIABLE(INTEGRATION_POINT_WEIGHT)
+// Application variables creation: (see
+// solid_mechanics_application_variables.cpp)
+// Application Constructor:
+KratosMultiscaleROMApplication::KratosMultiscaleROMApplication()
+    : mSmallDisplacementBbarElement2D4N(
+          0, Element::GeometryType::Pointer(new Quadrilateral2D4<Node<3>>(
+                 Element::GeometryType::PointsArrayType(4)))),
+      mSmallDisplacementHpromElement2D4N(
+          0, Element::GeometryType::Pointer(new Quadrilateral2D4<Node<3>>(
+                 Element::GeometryType::PointsArrayType(4)))),
+      mSmallDisplacementBbarElement3D8N(
+          0, Element::GeometryType::Pointer(new Hexahedra3D8<Node<3>>(
+                 Element::GeometryType::PointsArrayType(8)))),
+      mSmallDisplacementHpromElement3D8N(
+          0, Element::GeometryType::Pointer(new Hexahedra3D8<Node<3>>(
+                 Element::GeometryType::PointsArrayType(8)))),
+      mUpdatedLagrangianFbarElement2D4N(
+          0, Element::GeometryType::Pointer(new Quadrilateral2D4<Node<3>>(
+                 Element::GeometryType::PointsArrayType(4)))),
+      mTotalLagrangianFbarElement2D4N(
+          0, Element::GeometryType::Pointer(new Quadrilateral2D4<Node<3>>(
+                 Element::GeometryType::PointsArrayType(4)))),
+      mMinimalKineticCondition2D3N(
+          0, Condition::GeometryType::Pointer(new Line2D3<Node<3>>(
+                 Condition::GeometryType::PointsArrayType(3)))),
+      mMinimalKineticCondition3D3N(
+          0, Condition::GeometryType::Pointer(new Line3D3<Node<3>>(
+                 Condition::GeometryType::PointsArrayType(3)))) {}
 
-        KRATOS_REGISTER_ELEMENT( "SmallDisplacementBbarElement2D4N", mSmallDisplacementBbarElement2D4N )
-        KRATOS_REGISTER_ELEMENT( "SmallDisplacementHpromElement2D4N", mSmallDisplacementHpromElement2D4N )
-        KRATOS_REGISTER_ELEMENT( "SmallDisplacementBbarElement3D8N", mSmallDisplacementBbarElement3D8N )
-        KRATOS_REGISTER_ELEMENT( "SmallDisplacementHpromElement3D8N", mSmallDisplacementHpromElement3D8N )
-        KRATOS_REGISTER_ELEMENT( "UpdatedLagrangianFbarElement2D4N", mUpdatedLagrangianFbarElement2D4N )
-        KRATOS_REGISTER_ELEMENT( "TotalLagrangianFbarElement2D4N", mTotalLagrangianFbarElement2D4N )
+void KratosMultiscaleROMApplication::Register() {
+  // calling base class register to register Kratos components
+  KratosApplication::Register();
+  std::cout << "Initializing KratosMultiscaleROMApplication...  " << std::endl;
 
-        KRATOS_REGISTER_CONDITION( "MinimalKineticCondition2D3N", mMinimalKineticCondition2D3N)
-        KRATOS_REGISTER_CONDITION( "MinimalKineticCondition3D3N", mMinimalKineticCondition3D3N)
+  // Register Variables (variables created in
+  // solid_mechanics_application_variables.cpp)
+  KRATOS_REGISTER_VARIABLE(ISOTROPIC_DAMAGE_MODULUS)
+  KRATOS_REGISTER_VARIABLE(INFINITY_YIELD_STRESS)
+  KRATOS_REGISTER_VARIABLE(FLOW_RULE_IS_TRACTION_ONLY)
+  KRATOS_REGISTER_VARIABLE(LAGRANGE_MULTIPLIER_1)
+  KRATOS_REGISTER_VARIABLE(LAGRANGE_MULTIPLIER_2)
+  KRATOS_REGISTER_VARIABLE(LAGRANGE_MULTIPLIER_3)
+  KRATOS_REGISTER_VARIABLE(INITIAL_STRAIN_VECTOR)
+  KRATOS_REGISTER_VARIABLE(NUMBER_REDUCED_MODES)
+  KRATOS_REGISTER_VARIABLE(REDUCED_MODES_MATRIX)
+  KRATOS_REGISTER_VARIABLE(REDUCED_MODES_WEIGHTS)
+  KRATOS_REGISTER_VARIABLE(INTEGRATION_POINT_WEIGHT)
 
-        //Register Constitutive Laws
-        Serializer::Register("LinearIsotropicDamagePlaneStrain2DLaw", mLinearIsotropicDamagePlaneStrain2DLaw);
-        Serializer::Register("ExponentialIsotropicDamagePlaneStrain2DLaw", mExponentialIsotropicDamagePlaneStrain2DLaw);
-        Serializer::Register("LinearElasticPlasticJ2PlaneStrain2DLaw", mLinearElasticPlasticJ2PlaneStrain2DLaw);
-        Serializer::Register("SmallDisplacementElastoPlasticJ23DLaw", mSmallDisplacementElastoPlasticJ23DLaw);
+  KRATOS_REGISTER_ELEMENT("SmallDisplacementBbarElement2D4N",
+                          mSmallDisplacementBbarElement2D4N)
+  KRATOS_REGISTER_ELEMENT("SmallDisplacementHpromElement2D4N",
+                          mSmallDisplacementHpromElement2D4N)
+  KRATOS_REGISTER_ELEMENT("SmallDisplacementBbarElement3D8N",
+                          mSmallDisplacementBbarElement3D8N)
+  KRATOS_REGISTER_ELEMENT("SmallDisplacementHpromElement3D8N",
+                          mSmallDisplacementHpromElement3D8N)
+  KRATOS_REGISTER_ELEMENT("UpdatedLagrangianFbarElement2D4N",
+                          mUpdatedLagrangianFbarElement2D4N)
+  KRATOS_REGISTER_ELEMENT("TotalLagrangianFbarElement2D4N",
+                          mTotalLagrangianFbarElement2D4N)
 
-    }
+  KRATOS_REGISTER_CONDITION("MinimalKineticCondition2D3N",
+                            mMinimalKineticCondition2D3N)
+  KRATOS_REGISTER_CONDITION("MinimalKineticCondition3D3N",
+                            mMinimalKineticCondition3D3N)
+
+  // Register Constitutive Laws
+  Serializer::Register("LinearIsotropicDamagePlaneStrain2DLaw",
+                       mLinearIsotropicDamagePlaneStrain2DLaw);
+  Serializer::Register("ExponentialIsotropicDamagePlaneStrain2DLaw",
+                       mExponentialIsotropicDamagePlaneStrain2DLaw);
+  Serializer::Register("LinearElasticPlasticJ2PlaneStrain2DLaw",
+                       mLinearElasticPlasticJ2PlaneStrain2DLaw);
+  Serializer::Register("SmallDisplacementElastoPlasticJ23DLaw",
+                       mSmallDisplacementElastoPlasticJ23DLaw);
 }
-
+}
--- multiscale_rom_application.h
+++ multiscale_rom_application.h
@@ -1,126 +1,74 @@
-#if !defined(KRATOS_MULTISCALE_ROM_APPLICATION_H_INCLUDED )
-#define  KRATOS_MULTISCALE_ROM_APPLICATION_H_INCLUDED
+#if !defined(KRATOS_MULTISCALE_ROM_APPLICATION_H_INCLUDED)
+#define KRATOS_MULTISCALE_ROM_APPLICATION_H_INCLUDED
 
-// System includes
-#include <string>
 #include <iostream>
-// External includes
-// Project includes
-#include "includes/define.h"
+#include <string>
+
 #include "includes/constitutive_law.h"
-#include "includes/ublas_interface.h"
+#include "includes/define.h"
 #include "includes/kratos_application.h"
+#include "includes/ublas_interface.h"
 
 #include "containers/flags.h"
 
-#include "multiscale_rom_application_variables.h"
 #include "custom_conditions/minimal_kinetic_2D.hpp"
-#include "custom_elements/small_displacement_bbar_element.hpp"
-#include "custom_elements/small_displacement_hprom_element.hpp"
-#include "custom_elements/updated_lagrangian_fbar_element.hpp"
-#include "custom_elements/total_lagrangian_fbar_element.hpp"
-#include "custom_constitutive/linear_isotropic_damage_plane_strain_2D_law.hpp"
 #include "custom_constitutive/exponential_isotropic_damage_plane_strain_2D_law.hpp"
 #include "custom_constitutive/linear_elastic_plastic_J2_plane_strain_2D_law.hpp"
+#include "custom_constitutive/linear_isotropic_damage_plane_strain_2D_law.hpp"
 #include "custom_constitutive/small_displacement_elasto_plastic_J2_3D_law.hpp"
+#include "custom_elements/small_displacement_bbar_element.hpp"
+#include "custom_elements/small_displacement_hprom_element.hpp"
+#include "custom_elements/total_lagrangian_fbar_element.hpp"
+#include "custom_elements/updated_lagrangian_fbar_element.hpp"
+#include "multiscale_rom_application_variables.h"
 
 namespace Kratos {
-    ///@name Type Definitions
-    ///@{
-    typedef array_1d<double,3> Vector3;
-    typedef array_1d<double,6> Vector6;
-    ///@}
+typedef array_1d<double, 3> Vector3;
+class KratosMultiscaleROMApplication : public KratosApplication {
+public:
+  KRATOS_CLASS_POINTER_DEFINITION(KratosMultiscaleROMApplication);
+  KratosMultiscaleROMApplication();
+  virtual ~KratosMultiscaleROMApplication() {}
+  virtual void Register();
+  virtual std::string Info() const { return "KratosMultiscaleROMApplication"; }
+  virtual void PrintInfo(std::ostream &rOStream) const {
+    rOStream << Info();
+    PrintData(rOStream);
+  }
+  virtual void PrintData(std::ostream &rOStream) const {
+    KRATOS_WATCH("in KratosMultiscaleROMApplication")
+    KRATOS_WATCH(KratosComponents<VariableData>::GetComponents().size())
+    rOStream << "Variables:" << std::endl;
+    KratosComponents<VariableData>().PrintData(rOStream);
+    rOStream << std::endl;
+    rOStream << "Elements:" << std::endl;
+    KratosComponents<Element>().PrintData(rOStream);
+    rOStream << std::endl;
+    rOStream << "Conditions:" << std::endl;
+    KratosComponents<Condition>().PrintData(rOStream);
+  }
 
-    ///@name Kratos Globals
-    ///@{
-    
-    //Application variables definition:  (see solid_mechanics_application_variables.h)
-    
-    ///@}
-    ///@name Type Definitions
-    ///@{
-    
-    ///@}
-    ///@name  Enum's
-    ///@{
-    
-    ///@}
-    ///@name  Functions
-    ///@{
-    
-    ///@}
-    ///@name Kratos Classes
-    ///@{
-    
-    /// Short class definition.
-    /** Detail class definition.
-     */
-    class KratosMultiscaleROMApplication : public KratosApplication {
-       public:
-           /// Pointer definition of KratosSolidMechanicsApplication
-           KRATOS_CLASS_POINTER_DEFINITION(KratosMultiscaleROMApplication);
-           
-           /// Default constructor.
-           KratosMultiscaleROMApplication();
-           
-           /// Destructor.
-           virtual ~KratosMultiscaleROMApplication() {}
-           
-           virtual void Register();
-           
-           /// Turn back information as a string.
-           virtual std::string Info() const
-           {
-             return "KratosMultiscaleROMApplication";
-           }
-           
-           /// Print information about this object.
-           virtual void PrintInfo(std::ostream& rOStream) const
-           {
-             rOStream << Info();
-             PrintData(rOStream);
-           }
-           
-           ///// Print object's data.
-           virtual void PrintData(std::ostream& rOStream) const
-           {
-             KRATOS_WATCH( "in KratosMultiscaleROMApplication" )
-             KRATOS_WATCH( KratosComponents<VariableData>::GetComponents().size() )
-             rOStream << "Variables:" << std::endl;
-             KratosComponents<VariableData>().PrintData(rOStream);
-             rOStream << std::endl;
-             rOStream << "Elements:" << std::endl;
-             KratosComponents<Element>().PrintData(rOStream);
-             rOStream << std::endl;
-             rOStream << "Conditions:" << std::endl;
-             KratosComponents<Condition>().PrintData(rOStream);
-           }
-           
-       protected:
-       
-       private:
-           //elements
-           const SmallDisplacementBbarElement mSmallDisplacementBbarElement2D4N;
-           const SmallDisplacementHpromElement mSmallDisplacementHpromElement2D4N;
-           const SmallDisplacementBbarElement mSmallDisplacementBbarElement3D8N;
-           const SmallDisplacementHpromElement mSmallDisplacementHpromElement3D8N;
-           const UpdatedLagrangianFbarElement mUpdatedLagrangianFbarElement2D4N;
-           const TotalLagrangianFbarElement mTotalLagrangianFbarElement2D4N;
-           //conditions
-           const MinimalKineticCondition2D mMinimalKineticCondition2D3N;
-           const MinimalKineticCondition2D mMinimalKineticCondition3D3N;
-           //constitutive laws
-           const LinearIsotropicDamagePlaneStrain2DLaw mLinearIsotropicDamagePlaneStrain2DLaw;
-           const ExponentialIsotropicDamagePlaneStrain2DLaw mExponentialIsotropicDamagePlaneStrain2DLaw;
-           const LinearElasticPlasticJ2PlaneStrain2DLaw mLinearElasticPlasticJ2PlaneStrain2DLaw;
-           const SmallDisplacementElastoPlasticJ23DLaw mSmallDisplacementElastoPlasticJ23DLaw;
-           /// Assignment operator.
-           KratosMultiscaleROMApplication& operator=(KratosMultiscaleROMApplication const& rOther);
-           /// Copy constructor.
-           KratosMultiscaleROMApplication(KratosMultiscaleROMApplication const& rOther);
-    };
+protected:
+private:
+  const SmallDisplacementBbarElement mSmallDisplacementBbarElement2D4N;
+  const SmallDisplacementHpromElement mSmallDisplacementHpromElement2D4N;
+  const SmallDisplacementBbarElement mSmallDisplacementBbarElement3D8N;
+  const SmallDisplacementHpromElement mSmallDisplacementHpromElement3D8N;
+  const UpdatedLagrangianFbarElement mUpdatedLagrangianFbarElement2D4N;
+  const TotalLagrangianFbarElement mTotalLagrangianFbarElement2D4N;
+  const MinimalKineticCondition2D mMinimalKineticCondition2D3N;
+  const MinimalKineticCondition2D mMinimalKineticCondition3D3N;
+  const LinearIsotropicDamagePlaneStrain2DLaw
+      mLinearIsotropicDamagePlaneStrain2DLaw;
+  const ExponentialIsotropicDamagePlaneStrain2DLaw
+      mExponentialIsotropicDamagePlaneStrain2DLaw;
+  const LinearElasticPlasticJ2PlaneStrain2DLaw
+      mLinearElasticPlasticJ2PlaneStrain2DLaw;
+  const SmallDisplacementElastoPlasticJ23DLaw
+      mSmallDisplacementElastoPlasticJ23DLaw;
+  KratosMultiscaleROMApplication &
+  operator=(KratosMultiscaleROMApplication const &rOther);
+  KratosMultiscaleROMApplication(KratosMultiscaleROMApplication const &rOther);
+};
 }
-
-#endif // KRATOS_SOLID_MECHANICS_APPLICATION_H_INCLUDED  defined 
-
-
+#endif
