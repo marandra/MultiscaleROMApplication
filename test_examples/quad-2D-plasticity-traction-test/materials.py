96b1b85d46e94bd51c53dc0a0b2ea041cc5a9fe4
Manuel Caicedo <macaicedos@gmail.com>
2017-03-29 18:09:28 +0200

    add new validation tests- benchmarks

---
+++ test_examples/quad-2D-plasticity-traction-test/materials.py
@@ -0,0 +1,15 @@
+from __future__ import print_function, absolute_import, division #makes KratosMultiphysics backward compatible with python 2.6 and 2.7
+from KratosMultiphysics import *
+from KratosMultiphysics.SolidMechanicsApplication import *
+from KratosMultiphysics.MultiscaleROMApplication import *
+
+
+def AssignMaterial(Properties):
+
+    #mat = LinearElasticPlaneStrain2DLaw()
+    mat = LinearElasticPlasticJ2PlaneStrain2DLaw()
+    #mat = LinearIsotropicDamagePlaneStrain2DLaw()
+
+    Properties[1].SetValue(CONSTITUTIVE_LAW, mat.Clone())
+    Properties[2].SetValue(CONSTITUTIVE_LAW, mat.Clone())
+        
