fb9caff748c4a7b3c4dee5859f37201264b9fb9b
Marcelo Raschi <mraschi@cimne.upc.edu>
2017-05-29 12:16:56 +0200

    Added write mode (ascii/binary) and write frequency to process

--- python_scripts/write_elements_output_scalar.py
+++ python_scripts/write_elements_output_scalar.py
@@ -19,23 +19,46 @@ class WriteElementsOutputScalar(km.Process):
     def __init__(self, param, Model):
         self.model_part = Model[param['model_part_name'].GetString()]
         self.filename = param['filename'].GetString()
-        self.vname = param['variable_name'].GetString()
-        f = operator.attrgetter(self.vname)
-        self.Var = f(km)
+        self.write_frequency = param['write_frequency'].GetString()
+        self.write_mode = param['write_mode'].GetString()
+        self.var_reach = param['variable_reach'].GetString()
+        self.var_name = param['variable_name'].GetString()
+        f = operator.attrgetter(self.var_name)
+        if self.var_reach == "core":
+            self.Var = f(km)
+        else:
+            import KratosMultiphysics.MultiscaleROMApplication as msr
+            self.Var = f(msr)
 
     def write_results(self, filename):
-        #with open(filename, 'w') as ofile:
-        with open(filename, 'wb') as ofile:
-            process_info = self.model_part.ProcessInfo
-            for elem in self.model_part.Elements:
-                variables = elem.GetValuesOnIntegrationPoints(self.Var, process_info)
-                for v in variables:
-                    #ofile.write(" {:18.16f}\n".format(v[0]))
-                    ofile.write(struct.pack('f', v[0])) #  'f'=float32
-            ofile.write(b'\n')
+
+        def write_results_binary():
+            with open(filename, 'wb') as ofile:
+                process_info = self.model_part.ProcessInfo
+                for elem in self.model_part.Elements:
+                    variables = elem.GetValuesOnIntegrationPoints(self.Var, process_info)
+                    for v in variables:
+                        ofile.write(struct.pack('f', v[0])) # 'f'=float32
+                ofile.write(b'\n')
+
+        def write_results_ascii():
+            with open(filename, 'w') as ofile:
+                process_info = self.model_part.ProcessInfo
+                for elem in self.model_part.Elements:
+                    variables = elem.GetValuesOnIntegrationPoints(self.Var, process_info)
+                    for v in variables:
+                        ofile.write("{:18.16f}\n".format(v[0]))
+
+        if self.write_mode == "binary":
+            write_results_binary()
+        else:
+            write_results_ascii()
 
     def ExecuteInitialize(self):
-        pass
+        try:
+            os.remove(self.filename)
+        except OSError:
+            pass
 
     def ExecuteInitializeSolutionStep(self):
         self.timestep = "-{:.3f}".format(self.model_part.ProcessInfo[km.TIME])
@@ -54,7 +77,9 @@ class WriteElementsOutputScalar(km.Process):
         pass
 
     def ExecuteFinalizeSolutionStep(self):
-        self.write_results(self.filename + self.timestep)
+        if self.write_frequency == "every_timestep":
+            self.write_results(self.filename + self.timestep)
 
     def ExecuteFinalize(self):
-        pass
+        if self.write_frequency == "last_timestep":
+            self.write_results(self.filename)
