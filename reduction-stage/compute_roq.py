a9f45508e7e25a863d3d0c814071e0ef3fe46de2
Marcelo Raschi <mraschi@cimne.upc.edu>
2017-06-07 16:38:53 +0200

    updated parameters names and formatting

--- reduction-stage/compute_roq.py
+++ reduction-stage/compute_roq.py
@@ -2,72 +2,57 @@ import sys
 import configparser
 import glob
 import numpy as np
+import logging
 from compute_reduced_bases import ComputeROQ
 
-def read_modes(file_name):
-    c= np.loadtxt(file_name)
-    #print(c.shape)
-    return(c)
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+##handler = logging.FileHandler(sys.argv[1].rsplit('.', 1)[0] + '.log')
+##handler.setLevel(logging.INFO)
+##logger.addHandler(handler)
+
 
 # get parameters
-fname = sys.argv[1]
 conf = configparser.ConfigParser()
-conf.read(fname)
-file_format = conf['Parameters']['file_format']
+conf.read(sys.argv[1])
 nr_elements = int(conf['Parameters']['nr_elements'])
 nr_integration_points = int(conf['Parameters']['nr_integration_points'])
-#nr_energy_reduced_modes = int(conf['Parameters']['nr_energy_reduced_modes'])
-gaussweights_file_name = conf['Parameters']['gaussweights_file_name']
+nr_energy_reduced_modes = int(conf['Parameters']['nr_energy_reduced_modes'])
+integration_weights_filename = conf['Parameters']['integration_weights_filename']
+energy_bases_filename = conf['Parameters']['energy_bases_filename']
+trajectory_filename = conf['Parameters']['trajectory_filename']
+roq_weights_filename = conf['Parameters']['roq_weights_filename']
 
-# get files
-trajectory_paths = glob.glob('*_?')
+trajectory_paths = sorted(glob.glob("{}_?".format(trajectory_filename)))
+integration_weights_files = glob.glob("{}_?/{}".format(trajectory_filename, integration_weights_filename))
 
-gaussweights_file    = glob.glob(trajectory_paths[0] + '/' + gaussweights_file_name)
-#print(gaussweights_file)
+logger.info("REDUCED ORDER QUADRATURE")
 
-GaussWeights = np.fromfile(gaussweights_file[0], dtype=np.float32)
-
-# Read the energy modes from the txt file
-energy_basis_file    = './Output/EnergyBasis.dat'
-energy_modes = read_modes(energy_basis_file)
-#print(energy_modes.shape)
-
-print("************************")
-print("REDUCED ORDER QUADRATURE")
-print("************************")
-
-#num_ener_modes=100
-nr_energy_reduced_modes = 100
-
-#Ue_red=energy_modes[:,0:num_ener_modes]
+integration_weights = np.loadtxt(integration_weights_files[0])
+energy_modes = np.loadtxt(energy_bases_filename)
 if nr_energy_reduced_modes > energy_modes.shape[1]:
     sys.exit("Error: number of energy modes greater than the total number of computed energy modes")
-else:
-    num_ener_modes = nr_energy_reduced_modes
+energy_modes_red = energy_modes[:, 0:nr_energy_reduced_modes]
 
-energy_modes_red=energy_modes[:,0:num_ener_modes]
-
-factorLEQ=1.0
+factorLEQ = 1.0
 tol = 1e-10
-nGP=energy_modes.shape[1] #In case of use the same number of points as energy modes.
-#nGP=num_ener_modes #In case of use the same number of points as energy modes.
+nGP = energy_modes.shape[1] #In case of use the same number of points as energy modes.
 
-# TODO: Define a criteron to choose a subset of modes, or define a value (number of modes) as an input
-[w,z] = ComputeROQ(energy_modes_red, GaussWeights, factorLEQ, nGP, tol)
+# TODO: Define a criteron to choose a
+#  subset of modes, or define a value (number of modes) as an input
+[w,z] = ComputeROQ(energy_modes_red, integration_weights, factorLEQ, nGP, tol)
 
 # Print matrix with new weigths
-roq_weigths=np.empty([nr_elements, nr_integration_points])
+roq_weigths = np.empty([nr_elements, nr_integration_points])
 for i in range(nr_elements):
     for j in range(nr_integration_points):
-        i_elem=nr_integration_points*(i)+j
-        d=np.where(z==i_elem)[0]
+        i_elem = nr_integration_points * i + j
+        d = np.where(z==i_elem)[0]
         if not d:
-            roq_weigths[i][j]=-1.0
+            roq_weigths[i][j] = -1.0
         else:
-            roq_weigths[i][j]=w[d[0]]
+            roq_weigths[i][j] = w[d[0]]
 
-file_roq_weights='./Output/weights.dat'
-with open(file_roq_weights,'wb') as ofile_roq_weights:
-    np.savetxt(ofile_roq_weights, roq_weigths, fmt='%.13f')
+with open(roq_weights_filename,'wb') as of:
+    np.savetxt(of, roq_weigths, fmt='%.13f')
 
-#print("OK")
