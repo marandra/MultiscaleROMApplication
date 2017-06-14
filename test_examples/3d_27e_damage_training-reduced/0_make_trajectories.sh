ae6a3609f1f07eed29f70cf2b9bcc3ffbeaa45ea
Marcelo Raschi <mraschi@cimne.upc.edu>
2017-06-14 12:47:25 +0200

    fixed case script

--- test_examples/3d_27e_damage_training-reduced/0_make_trajectories.sh
+++ test_examples/3d_27e_damage_training-reduced/0_make_trajectories.sh
@@ -1,13 +1,15 @@
-IS=0.001
-m4 -DM4VAR_INITIALSTRAIN="[$IS, 0.0, 0.0, 0.0]" t_ProjectParameters.m4 > ProjectParameters_0.json
-m4 -DM4VAR_INITIALSTRAIN="[0.0, $IS, 0.0, 0.0]" t_ProjectParameters.m4 > ProjectParameters_1.json
-m4 -DM4VAR_INITIALSTRAIN="[0.0, 0.0, $IS, 0.0]" t_ProjectParameters.m4 > ProjectParameters_2.json
-m4 -DM4VAR_INITIALSTRAIN="[0.0, 0.0, 0.0, $IS]" t_ProjectParameters.m4 > ProjectParameters_3.json
-m4 -DM4VAR_INITIALSTRAIN="[$IS, $IS, 0.0, 0.0]" t_ProjectParameters.m4 > ProjectParameters_4.json
-m4 -DM4VAR_INITIALSTRAIN="[0.0, 0.0, $IS, $IS]" t_ProjectParameters.m4 > ProjectParameters_5.json
-m4 -DM4VAR_INITIALSTRAIN="[$IS, $IS, $IS, $IS]" t_ProjectParameters.m4 > ProjectParameters_6.json
+IS=0.02
+m4 -DM4VAR_INITIALSTRAIN="[$IS, 0.0, 0.0, 0.0, 0.0, 0.0]" t_ProjectParameters.m4 > ProjectParameters_0.json
+m4 -DM4VAR_INITIALSTRAIN="[0.0, $IS, 0.0, 0.0, 0.0, 0.0]" t_ProjectParameters.m4 > ProjectParameters_1.json
+m4 -DM4VAR_INITIALSTRAIN="[0.0, 0.0, $IS, 0.0, 0.0, 0.0]" t_ProjectParameters.m4 > ProjectParameters_2.json
+m4 -DM4VAR_INITIALSTRAIN="[0.0, 0.0, 0.0, $IS, 0.0, 0.0]" t_ProjectParameters.m4 > ProjectParameters_3.json
+m4 -DM4VAR_INITIALSTRAIN="[0.0, 0.0, 0.0, 0.0, $IS, 0.0]" t_ProjectParameters.m4 > ProjectParameters_4.json
+m4 -DM4VAR_INITIALSTRAIN="[0.0, 0.0, 0.0, 0.0, 0.0, $IS]" t_ProjectParameters.m4 > ProjectParameters_5.json
+m4 -DM4VAR_INITIALSTRAIN="[$IS, $IS, $IS, 0.0, 0.0, 0.0]" t_ProjectParameters.m4 > ProjectParameters_6.json
+m4 -DM4VAR_INITIALSTRAIN="[0.0, 0.0, 0.0, $IS, $IS, $IS]" t_ProjectParameters.m4 > ProjectParameters_7.json
+m4 -DM4VAR_INITIALSTRAIN="[$IS, $IS, $IS, $IS, $IS, $IS]" t_ProjectParameters.m4 > ProjectParameters_8.json
 
-for i in 0 1 2 3 4 5 6
+for i in 0 1 2 3 4 5 6 7 8
 do
 	TRAJ="trajectory_$i"
 	echo $TRAJ 
