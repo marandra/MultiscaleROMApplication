39c1effb79779621bac44fd3619e8ed39314fe6f
Marcelo Raschi <mraschi@cimne.upc.edu>
2017-06-26 15:14:36 +0200

    debugging homogenization

--- python_scripts/write_elements_homogenized_output.py
+++ python_scripts/write_elements_homogenized_output.py
@@ -20,8 +20,9 @@ def homogenization_function(self):
 
     for e, elem in enumerate(self.model_part.Elements):
         values = elem.GetValuesOnIntegrationPoints(self.Var, self.model_part.ProcessInfo)
-        weights = elem.GetValuesOnIntegrationPoints(msr.GAUSS_WEIGHTS, self.model_part.ProcessInfo)
+        weights = elem.GetValuesOnIntegrationPoints(km.INTEGRATION_WEIGHT, self.model_part.ProcessInfo)
         weights = [x[0] for x in weights] # to unpack received list-inside-list
+        print("DEBUG WEIGHTS {}".format(weights))
         for i, w in enumerate(weights):
             # used in HPROM case, to ignore GP
             if w == -1:
