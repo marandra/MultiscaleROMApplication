f579896608c8fab8e60f407e46bd6e2ec50a904b
Marcelo Raschi <mraschi@cimne.upc.edu>
2017-06-27 15:46:52 +0200

    optimization of the reduced bases files

--- reduction-stage/compute_bases.py
+++ reduction-stage/compute_bases.py
@@ -1,160 +1,92 @@
-import sys
+import time
 import configparser
 import argparse
 import glob
 import numpy as np
 import logging
 
-def make_list_of_files(conf):
 
+def write_bases(filename, U):
+    bases_file_format = conf['Parameters']['bases_file_format']
+    if bases_file_format == 'ascii':
+        np.savetxt(filename, U)
+    else:
+        np.save(filename, U)
+    return
+
+
+def make_list_of_files(conf):
+    logger.debug("Scanning trajectories")
     trajectory_filename = conf['Parameters']['trajectory_filename']
-    nr_elastic_snapshots_filename = conf['Parameters']['nr_elastic_snapshots_filename']
+    nr_e_snap_filename = conf['Parameters']['nr_elastic_snapshots_filename']
     energy_filename = conf['Parameters']['energy_filename']
     strain_filename = conf['Parameters']['strain_filename']
-
     trajectory_paths = sorted(glob.glob("{}_?".format(trajectory_filename)))
-    energy_elastic_files = []
-    energy_inelast_files = []
-    strain_elastic_files = []
-    strain_inelast_files = []
+    ene_e_files = []
+    ene_i_files = []
+    str_e_files = []
+    str_i_files = []
     for path in trajectory_paths:
-        with open("{}/{}".format(path, nr_elastic_snapshots_filename), "r") as f:
-            nr_elastic_snapshots = int(f.readline().strip())
-        logger.info("Nr of elastic snapshots: {}".format(nr_elastic_snapshots))
-        energy_elastic_files.extend(sorted(glob.glob("{}/{}*".format(path, energy_filename)))[:nr_elastic_snapshots])
-        energy_inelast_files.extend(sorted(glob.glob("{}/{}*".format(path, energy_filename)))[nr_elastic_snapshots:])
-        strain_elastic_files.extend(sorted(glob.glob("{}/{}*".format(path, strain_filename)))[:nr_elastic_snapshots])
-        strain_inelast_files.extend(sorted(glob.glob("{}/{}*".format(path, strain_filename)))[nr_elastic_snapshots:])
+        with open("{}/{}".format(path, nr_e_snap_filename), "r") as f:
+            nr_e_snap = int(f.readline().strip())
+        logger.debug("  {} - elastic snapshots: {}".format(path, nr_e_snap))
+        ene_e_files.extend(sorted(glob.glob("{}/{}*".format(path, energy_filename)))[:nr_e_snap])
+        ene_i_files.extend(sorted(glob.glob("{}/{}*".format(path, energy_filename)))[nr_e_snap:])
+        str_e_files.extend(sorted(glob.glob("{}/{}*".format(path, strain_filename)))[:nr_e_snap])
+        str_i_files.extend(sorted(glob.glob("{}/{}*".format(path, strain_filename)))[nr_e_snap:])
+    logger.debug("")
+    return ene_e_files, ene_i_files, str_e_files, str_i_files
 
-    return energy_elastic_files, energy_inelast_files, strain_elastic_files, strain_inelast_files
 
-
-def compute_strain_elastic(conf, strain_elastic_files):
-    logger.info("SVD of strain elastic snapshots")
-
+def compute_inelastic_modes(conf, files, Ue, nr_components):
+    logger.info("  Projection of inelastic snapshots")
     nr_elements = int(conf['Parameters']['nr_elements'])
     nr_integration_points = int(conf['Parameters']['nr_integration_points'])
-    nr_strain_components = int(conf['Parameters']['nr_strain_components'])
-    tol_svd_elastic_strain = float(conf['Parameters']['tolerance_svd_elastic_strain'])
-
-    nr_dofs = nr_elements * nr_integration_points * nr_strain_components
-    X = np.empty([nr_dofs, len(strain_elastic_files)])
-    for i, file in enumerate(strain_elastic_files):
+    nr_modes = int(conf['Parameters']['max_nr_reduced_modes'])
+    nr_dofs = nr_elements * nr_integration_points * nr_components
+    X = np.empty([nr_dofs, len(files)])
+    for i, file in enumerate(files):
         X[:, i] = np.fromfile(file, dtype=np.float32)
-    # TODO: incluir el SVD del paquete scipy, parece que es mas optimo, ver si esta instalada una version reciente de scipy en el cluster
-    [U, S] = np.linalg.svd(X, full_matrices=False)[:2]
-    logger.info(S)
-
-    logger.info("selection of strain elastic modes")
-    cont = 1
-    Us_el = []
-    for iValue, singular_value in enumerate(S):
-        if singular_value > tol_svd_elastic_strain:
-            if cont == 1:
-                Us_el = U[:, iValue]
-            else:
-                Us_el = np.column_stack((Us_el, U[:, iValue]))
-            cont = cont + 1
-    logger.info(Us_el.shape)
-
-    return Us_el
+        for j in range(Ue.shape[1]):
+            X[:,i] = X[:,i] - np.multiply(np.dot(Ue[:, j], X[:, i]), Ue[:, j])
+    logger.info("  SVD of inelastic snapshots")
+    U = np.linalg.svd(X, full_matrices=False)[0]
+    Ur = U[:,:nr_modes]
+    logger.info("  - nr of modes: {}".format(Ur.shape[1]))
+    logger.info("  - size of mode: {}".format(Ur.shape[0]))
+    return Ur
 
 
-def compute_strain_inelastic(conf, strain_inelast_files, Us_el):
-    logger.info("projection of strain inelastic snapshots")
-
-    nr_elements = int(conf['Parameters']['nr_elements'])
-    nr_integration_points = int(conf['Parameters']['nr_integration_points'])
-    nr_strain_components = int(conf['Parameters']['nr_strain_components'])
-    tol_svd_inelastic_strain = float(conf['Parameters']['tolerance_svd_inelastic_strain'])
-
-    nr_dofs = nr_elements * nr_integration_points * nr_strain_components
-
-    X = np.empty([nr_dofs, len(strain_inelast_files)])
-    for i, file in enumerate(strain_inelast_files):
-        X[:, i] = np.fromfile(file, dtype=np.float32)
-        for j in range(Us_el.shape[1]):
-            X[:,i] = X[:,i] - np.multiply(np.dot(Us_el[:,j],X[:,i]),Us_el[:,j])
-
-    logger.info("SVD of strain inelastic modified snapshots")
-    [U, S] = np.linalg.svd(X, full_matrices=False)[:2]
-    logger.info(S)
-
-    logger.info("selection of strain inelastic modes")
-    cont = 1
-    Us_in = []
-    for iValue, singular_value in enumerate(S):
-        if singular_value > tol_svd_inelastic_strain:
-            if cont == 1:
-                Us_in = U[:, iValue]
-            else:
-                Us_in = np.column_stack((Us_in, U[:, iValue]))
-            cont = cont + 1
-    logger.info(Us_in.shape)
-
-    return Us_in
-
-
-def compute_energy_elastic(conf, energy_elastic_files):
-
-    logger.info("SVD of elastic energy snapshots")
-
+def compute_elastic_modes(conf, files, nr_components):
+    logger.info("  SVD of elastic snapshots")
     nr_elements = int(conf['Parameters']['nr_elements'])
     nr_integration_points = int(conf['Parameters']['nr_integration_points'])
     nr_strain_components = int(conf['Parameters']['nr_strain_components'])
     nr_modes = 2 * nr_strain_components
-
-    nr_dofs = nr_elements * nr_integration_points
-    X = np.empty([nr_dofs, len(energy_elastic_files)])
-    for i, file in enumerate(energy_elastic_files):
+    nr_dofs = nr_elements * nr_integration_points * nr_components
+    X = np.empty([nr_dofs, len(files)])
+    for i, file in enumerate(files):
         X[:, i] = np.fromfile(file, dtype=np.float32)
     [U, S] = np.linalg.svd(X, full_matrices=False)[:2]
-
-    Ue_el = U[:,:nr_modes]
-    logger.info(S[:nr_modes])
-    logger.info(Ue_el.shape)
-
-    return Ue_el
-
-
-def compute_energy_inelastic(conf, energy_inelast_files, Ue_el):
-
-    logger.info("projection of energy inelastic snapshots")
-
-    nr_elements = int(conf['Parameters']['nr_elements'])
-    nr_integration_points = int(conf['Parameters']['nr_integration_points'])
-    nr_modes = int(conf['Parameters']['max_nr_reduced_modes'])
-
-    nr_dofs = nr_elements * nr_integration_points
-
-    X = np.empty([nr_dofs, len(energy_inelast_files)])
-    for i, file in enumerate(energy_inelast_files):
-        X[:, i] = np.fromfile(file, dtype=np.float32)
-        for j in range(Ue_el.shape[1]):
-            X[:,i] = X[:,i] - np.multiply(np.dot(Ue_el[:,j],X[:,i]),Ue_el[:,j])
-
-    logger.info("SVD of inelastic energy modified snapshots")
-    [U, S] = np.linalg.svd(X, full_matrices=False)[:2]
-    Ue_in = U[:,:nr_modes]
-
-    logger.info(Ue_in.shape)
-
-    return Ue_in
+    Ur = U[:,:nr_modes]
+    logger.debug("  - singular values of selected modes: {}".format(S[:nr_modes]))
+    logger.info("  - nr of modes: {}".format(Ur.shape[1]))
+    logger.info("  - size of mode: {}".format(Ur.shape[0]))
+    return Ur
 
 
 #######################################
 # Main
 #######################################
-logging.basicConfig(level=logging.INFO)
+#logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
-
 parser = argparse.ArgumentParser(description="Computes energy and strain reduced bases.")
 parser.add_argument('config_file', help="configuration file")
 group = parser.add_mutually_exclusive_group()
 group.add_argument('-e', '--only-energy', action="store_true", help="compute only energy reduced bases")
 group.add_argument('-s', '--only-strain', action="store_true", help="compute only strain reduced bases")
 args = parser.parse_args()
-
 flag_comp_energy = True
 flag_comp_strain =  True
 if args.only_energy:
@@ -165,34 +97,32 @@ elif args.only_strain:
 if __name__ == '__main__':
     conf = configparser.ConfigParser()
     conf.read(args.config_file)
-    energy_bases_filename = conf['Parameters']['energy_bases_filename']
-    strain_bases_filename = conf['Parameters']['strain_bases_filename']
+    ene_bases_fname = conf['Parameters']['energy_bases_filename']
+    str_bases_fname = conf['Parameters']['strain_bases_filename']
     bases_file_format = conf['Parameters']['bases_file_format']
-
-    energy_elastic_files, energy_inelast_files, strain_elastic_files, strain_inelast_files = make_list_of_files(conf)
+    ene_e_files, ene_i_files, str_e_files, str_i_files = make_list_of_files(conf)
 
     if flag_comp_energy:
-        logger.info("Computing energy snapshots")
-        Ue_el = compute_energy_elastic(conf, energy_elastic_files)
-        Ue_in = compute_energy_inelastic(conf, energy_inelast_files, Ue_el)
-        Ue = np.hstack([Ue_el, Ue_in])
-        if bases_file_format == 'ascii':
-            with open(energy_bases_filename, 'wb') as ofile:
-                np.savetxt(ofile, Ue, fmt='%.17f')
-        else:
-            logger.info("binary output not implemented yet. writing in ascii")
-            with open(energy_bases_filename, 'wb') as ofile:
-                np.savetxt(ofile, Ue, fmt='%.17f')
-    if flag_comp_strain:
-        logger.info("Computing strain bases")
-        Us_el = compute_strain_elastic(conf, strain_elastic_files)
-        Us_in = compute_strain_inelastic(conf, strain_inelast_files, Us_el)
-        Us = np.hstack([Us_el, Us_in])
-        if bases_file_format == 'ascii':
-            with open(strain_bases_filename, 'wb') as ofile:
-                np.savetxt(ofile, Us, fmt='%.17f')
-        else:
-            logger.info("binary output not implemented yet. writing in ascii")
-            with open(strain_bases_filename, 'wb') as ofile:
-                np.savetxt(ofile, Us, fmt='%.17f')
+        t0 = time.time()
+        logger.info("Generating energy bases")
+        Ue = compute_elastic_modes(conf, ene_e_files, nr_components=1)
+        Ui = compute_inelastic_modes(conf, ene_i_files, Ue, nr_components=1)
+        U = np.hstack([Ue, Ui])
+        t1 = time.time()
+        write_bases(ene_bases_fname, U)
+        logger.info("  SVD time: {:.1f}s".format(time.time() - t0))
+        logger.info("  Writing time: {:.1f}s".format(time.time() - t1))
+        logger.info("")
 
+    if flag_comp_strain:
+        t0 = time.time()
+        logger.info("Generation strain bases")
+        nr_strain_components = int(conf['Parameters']['nr_strain_components'])
+        Ue = compute_elastic_modes(conf, str_e_files, nr_components=nr_strain_components)
+        Ui = compute_inelastic_modes(conf, str_i_files, Ue, nr_components=nr_strain_components)
+        U = np.hstack([Ue, Ui])
+        t1 = time.time()
+        write_bases(str_bases_fname, U)
+        logger.info("  SVD time: {:.1f}s".format(time.time() - t0))
+        logger.info("  Writing time: {:.1f}s".format(time.time() - t1))
+        logger.info("")
