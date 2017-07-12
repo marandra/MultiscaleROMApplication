1fc527b93490e9155f5e45e945962df43383bbd6
Marcelo Raschi <mraschi@cimne.upc.edu>
2017-07-12 19:02:01 +0200

    finishing hprom solver in python. WIP.

--- reduction-stage/hprom_solver.py
+++ reduction-stage/hprom_solver.py
@@ -2,28 +2,33 @@
 from __future__ import print_function, absolute_import, division
 import KratosMultiphysics as km
 import read_materials_process
-import KratosMultiphysics.MultiscaleROMApplication as msr
+#import KratosMultiphysics.MultiscaleROMApplication as msr
 import numpy as np
 import json
-import sys
+import time as timer
+import copy
 
 
 def kratos_to_numpy_matrix(K, m, n):
     N = np.empty((m, n))
     for i in range(m):
         for j in range(n):
-            N[i,j] = K[i,j]
+            N[i,j] = copy.copy(K[i,j])
     return N
 
 
 def kratos_to_numpy_vector(K, n):
+    print("Debug: {}".format(n))
     N = np.empty(n)
     for i in range(n):
-        N[i] = K[i]
+        N[i] = copy.copy(K[i])
     return N
 
 
-def _call_cl(epsilon_h, cl, geom, process_info, properties):
+def _call_cl(epsilon, cl, geom, process_info, properties):
+
+    N = km.Vector(3)
+    #cl.InitializeMaterial(properties, geom, N)
     cl.Check(properties, geom, process_info)
     cl_options = km.Flags()
     cl_options.Set(km.ConstitutiveLaw.COMPUTE_STRAIN, False)
@@ -31,19 +36,18 @@ def _call_cl(epsilon_h, cl, geom, process_info, properties):
     cl_options.Set(km.ConstitutiveLaw.COMPUTE_CONSTITUTIVE_TENSOR, True)
 
     F = km.Matrix(3,3)
-    F[0,0] = 1.0 + epsilon_h[0];   F[0,1] = 0.5 * epsilon_h[3]; F[0,2] = 0.5 * epsilon_h[5]
-    F[1,0] = 0.5 * epsilon_h[3];   F[1,1] = 1.0 + epsilon_h[1]; F[1,2] = 0.5 * epsilon_h[4]
-    F[2,0] = 0.5 * epsilon_h[5];   F[2,1] = 0.5 * epsilon_h[4]; F[2,2] = 1.0 + epsilon_h[2]
+    F[0,0] = 1.0 + epsilon[0];   F[0,1] = 0.5 * epsilon[3]; F[0,2] = 0.5 * epsilon[5]
+    F[1,0] = 0.5 * epsilon[3];   F[1,1] = 1.0 + epsilon[1]; F[1,2] = 0.5 * epsilon[4]
+    F[2,0] = 0.5 * epsilon[5];   F[2,1] = 0.5 * epsilon[4]; F[2,2] = 1.0 + epsilon[2]
     FN = kratos_to_numpy_matrix(F, 3, 3)
     detF = np.linalg.det(FN)
-    N = km.Vector(3)
     DN_DX = km.Matrix(3,2)
     constitutive_matrix = km.Matrix(cl.GetStrainSize(), cl.GetStrainSize())
     stress_vector = km.Vector(cl.GetStrainSize())
     strain_vector = km.Vector(cl.GetStrainSize())
     for i in range(cl.GetStrainSize()):
         stress_vector[i] = 0.
-        strain_vector[i] = epsilon_h[i]
+        strain_vector[i] = epsilon[i]
 
     #setting the parameters - note that a constitutive law may not need them all!
     cl_params = km.ConstitutiveLawParameters()
@@ -62,46 +66,79 @@ def _call_cl(epsilon_h, cl, geom, process_info, properties):
     cl.CalculateMaterialResponseCauchy(cl_params)
     #print("The Material Response Cauchy")
     #print( "stress = ", cl_params.GetStressVector() )
-    #print( "_train = ", cl_params.GetStrainVector() )
+    #print( "strain = ", cl_params.GetStrainVector() )
     #print( "C      = ", cl_params.GetConstitutiveMatrix() )
     #print("")
 
-    return  cl_params.GetStressVector(), cl_params.GetConstitutiveMatrix()
+    cl.FinalizeSolutionStep(properties, geom, N, process_info)
+
+    CM = cl_params.GetConstitutiveMatrix()
+    stress = cl_params.GetStressVector()
+    size = cl.GetStrainSize()
+    CM_np = np.empty((size, size))
+    stress_np = np.empty(size)
+    for i in range(size):
+        stress_np[i] = stress[i]
+        for j in range(size):
+            CM_np[i, j] = CM[i, j]
+
+    return stress_np, CM_np
+    #return  cl_params.GetStressVector(), cl_params.GetConstitutiveMatrix()
 
 
-def calculate_alpha(epsilon_h, iw_list, CL_list, B_list, props_list, model_part):
+def initialize(iw_list, CL_list, B_list, props_list, model_part, geom):
+    print("Initializing CLs")
+    for i, cl in enumerate(CL_list):
+        cl.InitializeMaterial(model_part.Properties[props_list[i]], geom, km.Vector(3))
+    print("")
 
+def calculate_residual(x, epsilon_h, iw_list, CL_list, B_list, props_list, model_part, geom):
+    nr_points = len(B_list)
+    #nr_comps = len(B_list[0])
+    nr_modes = len(B_list[0][0])
+    An = np.zeros((nr_modes, nr_modes))
+    bn = np.zeros(nr_modes)
+    for i in range(nr_points):
+        Bn = np.array(B_list[i])
+        epsilon = epsilon_h + np.dot(Bn, x)
+        sigma_n, C_n = _call_cl(epsilon, CL_list[i], geom, model_part.ProcessInfo, model_part.Properties[props_list[i]])
+        #Cn = kratos_to_numpy_matrix(C, nr_comps, nr_comps)
+        #print(C_n)
+        #print(np.dot(Bn.transpose(), np.dot(Cn, Bn)))
+        #print("")
+        #sigma_n = kratos_to_numpy_vector(sigma, nr_comps)
+        w = iw_list[i]
+        An += w * np.dot(Bn.transpose(), np.dot(C_n, Bn))
+        bn += w * np.dot(Bn.transpose(), sigma_n)
+    return An, bn
+
+
+def solve(x, epsilon_h, iw_list, CL_list, B_list, props_list, model_part, geom):
     nr_points = len(B_list)
     nr_comps = len(B_list[0])
     nr_modes = len(B_list[0][0])
 
-    node1 = model_part.CreateNewNode(1,0.0,0.0,0.0)
-    geom = km.Triangle2D3(node1, node1, node1)
-
-    N = km.Vector(3)
-
-    for i, cl in enumerate(CL_list):
-        cl.InitializeMaterial(model_part.Properties[props_list[i]], geom, N)
-
-    while(True):
-        An = np.zeros((nr_modes, nr_modes))
-        bn = np.zeros(nr_modes)
-        for i in range(nr_points):
-            sigma, C = _call_cl(epsilon_h, CL_list[i], geom, model_part.ProcessInfo, model_part.Properties[props_list[i]])
-            Bn = np.array(B_list[i])
-            Cn = kratos_to_numpy_matrix(C, nr_comps, nr_comps)
-            sigma_n = kratos_to_numpy_vector(sigma, nr_comps)
-            w = iw_list[i]
-            An += w * np.dot(Bn.transpose(), np.dot(Cn, Bn))
-            bn += w * np.dot(Bn.transpose(), sigma_n)
-
-        x = np.linalg.solve(An, bn)
-
-        print(An)
-        print(bn)
-        print(x)
-
-
+    it = 0
+    tol = 1e-9
+    A, res = calculate_residual(x, epsilon_h, iw_list, CL_list, B_list, props_list, model_part, geom)
+    norm_res = 1
+    #print("iteration = {}".format(it))
+    #print(A)
+    #print(x)
+    #print(res)
+    print("norm res: {:.3e}".format(norm_res))
+    it += 1
+    while(norm_res > tol and it < 10):
+        #print("iteration = {}".format(it))
+        x -= np.linalg.solve(A, res)
+        A, res = calculate_residual(x, epsilon_h, iw_list, CL_list, B_list, props_list, model_part, geom)
+        norm_res = np.linalg.norm(res, ord=2)
+        #print(A)
+        #print(x)
+        #print(res)
+        print("norm res: {:.3e}".format(norm_res))
+        it += 1
+    print("Convergence is achieved (or not)")
 
 ##############################################################
 if __name__ == "__main__":
@@ -117,16 +154,8 @@ if __name__ == "__main__":
             }
             """)
     read_materials_process.Factory(settings, Model)
-    print(model_part.Properties[1])
-    print(model_part.Properties[2])
-    #model_part.AddProperties(km.Properties(1))
-    #properties = model_part.Properties[1]
-    #properties.SetValue(km.YOUNG_MODULUS, 21000)
-    #properties.SetValue(km.POISSON_RATIO, 0.3)
-    #properties.SetValue(km.YIELD_STRESS, 5.5)
-    #properties.SetValue(msr.INFINITY_YIELD_STRESS, 100000)
-    #properties.SetValue(msr.ISOTROPIC_DAMAGE_MODULUS, 0.01)
-    #properties.SetValue(msr.FLOW_RULE_IS_TRACTION_ONLY, False)
+    #print(model_part.Properties[1])
+    #print(model_part.Properties[2])
 
     # get integration point weights
     with open("rve.json", "r") as fi:
@@ -138,10 +167,10 @@ if __name__ == "__main__":
     nr_points = len(B_list)
     nr_comps = len(B_list[0])
     nr_modes = len(B_list[0][0])
-
-    print(nr_points)
-    print(nr_comps)
-    print(nr_modes)
+    print("Nr points: {}".format(nr_points))
+    print("Nr components: {}".format(nr_comps))
+    print("Nr modes: {}".format(nr_modes))
+    print("")
 
     BK_list = []
     CL_list = []
@@ -153,14 +182,36 @@ if __name__ == "__main__":
         BK_list.append(BK)
         CL_list.append(model_part.Properties[props_list[i]][km.CONSTITUTIVE_LAW].Clone())
 
-    nr_timesteps
-    epsilon_h = km.Vector(nr_comps)
-    for t in range(nr_timesteps):
-        epsilon_h[0] = 0.00001 * t / nr_timesteps
-        epsilon_h[1] = 0.00000 * t / nr_timesteps
-        epsilon_h[2] = 0.00000 * t / nr_timesteps
-        epsilon_h[3] = 0.00000 * t / nr_timesteps
-        epsilon_h[4] = 0.00000 * t / nr_timesteps
-        epsilon_h[5] = 0.00000 * t / nr_timesteps
+    initial_strain = km.Vector(nr_comps)
+    initial_strain[0] = 0.001
+    initial_strain[1] = 0.
+    initial_strain[2] = 0.
+    initial_strain[3] = 0.
+    initial_strain[4] = 0.
+    initial_strain[5] = 0.
 
-        calculate_alpha(epsilon_h, iw_list, CL_list, B_list, props_list, model_part)
+    #setting of constant values
+    nr_points = len(B_list)
+    nr_comps = len(B_list[0])
+    nr_modes = len(B_list[0][0])
+    node1 = model_part.CreateNewNode(1,0.0,0.0,0.0)
+    geom = km.Triangle2D3(node1, node1, node1)
+
+    initialize(iw_list, CL_list, B_list, props_list, model_part, geom)
+    x = np.zeros(nr_modes)
+
+    t0 = timer.time()
+    nr_time_steps = 100
+    end_time = 1.
+    delta_time = end_time / nr_time_steps
+    time = delta_time
+    tolerance = delta_time / 10.
+    while(time <= end_time + tolerance):
+        print("")
+        print("Current time: {:.3f} --- Elapsed time: {:.2f}s".format(time, timer.time() - t0))
+        partial_initial_strain = (time / end_time) * initial_strain 
+        solve(x, partial_initial_strain, iw_list, CL_list, B_list, props_list, model_part, geom)
+        print(x)
+        time = time + delta_time
+
+    print("Computing time = {:.2f}s".format(timer.time() - t0))
