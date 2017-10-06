4aaea4d21509df530a8ceecfc9988b56ffdd50e0
Manuel Caicedo <mcaicedo@cimne.upc.edu>
2017-10-06 11:45:34 +0200

    Plasticity Constitutive law - changes on the computation of internal variables

--- custom_constitutive/linear_J2_plasticity_3D_law.hpp
+++ custom_constitutive/linear_J2_plasticity_3D_law.hpp
@@ -91,6 +91,7 @@ protected:
     double yieldFunction(const double, const Properties& rMaterialProperties);
     double GetDeltaGamma(double norm_s_trial, const Properties& rMaterialProperties);
     double GetSaturationHardening(const Properties& rMaterialProperties);
+    double GetPlasticPotential(const Properties& rMaterialProperties);
     virtual void CalculateTangentTensor(double dgamma,
                                         double norm_s_trial,
                                         const Vector& N_new,
