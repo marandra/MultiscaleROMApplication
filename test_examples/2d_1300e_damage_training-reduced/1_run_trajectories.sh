b81a4b1d881a6b5b729ee04f74964cdfa8e9e288
Marcelo Raschi <mraschi@cimne.upc.edu>
2017-11-03 17:29:19 +0100

    removed spaces

--- test_examples/2d_1300e_damage_training-reduced/1_run_trajectories.sh
+++ test_examples/2d_1300e_damage_training-reduced/1_run_trajectories.sh
@@ -2,7 +2,7 @@ export OMP_NUM_THREADS=1
 echo "OMP_NUM_THREADS="$OMP_NUM_THREADS
 for i in trajectory_?
 do
-       	cd $i
+    cd $i
 	echo $i
 	python3 MainKratos.py > outMainKratos 2>&1 &
 	cd ..
