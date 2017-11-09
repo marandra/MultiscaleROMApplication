bf9f8f5ff6259e46c3efcfdd0147a80d0fa3d119
Marcelo Raschi <mraschi@cimne.upc.edu>
2017-11-09 13:24:01 +0100

    added missing test case files

---
+++ test_examples/2d_1300e_damage_training-reduced/generate_analysis.py
@@ -0,0 +1,50 @@
+import os
+import time
+import main_kratos_hprom as kratos
+
+TRAJ = []
+TRAJ.append([0.0,   0.0,   0.001])  # 0
+TRAJ.append([0.0,   0.001, 0.0, ])  # 1
+TRAJ.append([0.001, 0.0,   0.0, ])  # 2
+TRAJ.append([0.001, 0.0,   0.0, ])  # 3
+TRAJ.append([0.001, 0.001, 0.0, ])  # 4
+TRAJ.append([0.0,   0.001, 0.001])  # 5
+TRAJ.append([0.001, 0.001, 0.001])  # 6
+
+
+if __name__ == "__main__":
+
+    if True:
+        # HPROM
+        for m in [10]:
+            for p in [10]:
+                filename = "bases/rve_{}_{}.json".format(m, p)
+                k = kratos.Kratos(filename=filename)
+                #for t in range(63):
+                for t in range(1):
+                    path_out ="traj_{}/m{}p{}".format(t, m, p)
+                    if not os.path.exists(path_out):
+                        os.makedirs(path_out)
+                    t1 = time.time()
+                    k.init_case(strain=TRAJ[t], path_out=path_out)
+                    k.run()                                     
+                    t2 = time.time()
+                    with open("{}/time.dat".format(path_out), 'w') as fo:
+                        fo.write("{:.2f}\n".format(t2 - t1))
+
+    if False:
+        # ROM
+        for m in [10]:
+            for p in ['ROM']:
+                filename = "bases/rve_{}_{}.json".format(m, p)
+                k = kratos.Kratos(filename=filename)
+                for t in [1]:
+                    path_out ="traj_{}/m{}p{}".format(t, m, p)
+                    if not os.path.exists(path_out):
+                        os.makedirs(path_out)
+                    t1 = time.time()
+                    k.init_case(strain=TRAJ[t], path_out=path_out)
+                    k.run()                                     
+                    t2 = time.time()
+                    with open("{}/time.dat".format(path_out), 'w') as fo:
+                        fo.write("{:.2f}\n".format(t2 - t1))
