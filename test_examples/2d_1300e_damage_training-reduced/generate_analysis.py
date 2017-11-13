88d44256a54ca7dcf7d1b2bcfac57cd84ee162ac
Marcelo <mraschi@cimne.upc.edu>
2017-11-13 20:04:56 +0100

    updated test case files

--- test_examples/2d_1300e_damage_training-reduced/generate_analysis.py
+++ test_examples/2d_1300e_damage_training-reduced/generate_analysis.py
@@ -3,25 +3,25 @@ import time
 import main_kratos_hprom as kratos
 
 TRAJ = []
-TRAJ.append([0.0,   0.0,   0.001])  # 0
+TRAJ.append([0.001, 0.0,   0.0, ])  # 0
 TRAJ.append([0.0,   0.001, 0.0, ])  # 1
-TRAJ.append([0.001, 0.0,   0.0, ])  # 2
-TRAJ.append([0.001, 0.0,   0.0, ])  # 3
-TRAJ.append([0.001, 0.001, 0.0, ])  # 4
+TRAJ.append([0.0,   0.0,   0.001])  # 2
+TRAJ.append([0.001, 0.001, 0.0, ])  # 3
+TRAJ.append([0.001, 0.0,   0.001])  # 4
 TRAJ.append([0.0,   0.001, 0.001])  # 5
 TRAJ.append([0.001, 0.001, 0.001])  # 6
 
 
 if __name__ == "__main__":
 
-    if True:
+    if False:
         # HPROM
         for m in [10]:
-            for p in [10]:
+            for p in [100]:
                 filename = "bases/rve_{}_{}.json".format(m, p)
                 k = kratos.Kratos(filename=filename)
                 #for t in range(63):
-                for t in range(1):
+                for t in  [1]:
                     path_out ="traj_{}/m{}p{}".format(t, m, p)
                     if not os.path.exists(path_out):
                         os.makedirs(path_out)
@@ -32,7 +32,7 @@ if __name__ == "__main__":
                     with open("{}/time.dat".format(path_out), 'w') as fo:
                         fo.write("{:.2f}\n".format(t2 - t1))
 
-    if False:
+    if True:
         # ROM
         for m in [10]:
             for p in ['ROM']:
