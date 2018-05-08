9e0247588e5963bf4d71977497a63384b75d9768
Marcelo <marcelo.raschi@gmail.com>
2018-05-08 11:39:08 +0200

    write total displacement (WIP)

--- python_scripts/write_nodal_total_displacement_process.py
+++ python_scripts/write_nodal_total_displacement_process.py
@@ -1,28 +1,28 @@
 import KratosMultiphysics as km
-import KratosMultiphysics.MultiscaleROMApplication as msr
-import os
-import struct
+#import KratosMultiphysics.MultiscaleROMApplication as msr
+#import os
+#import struct
 
 def Factory(settings, Model):
     if(type(settings) != km.Parameters):
         raise Exception("expected input is Parameters object, encapsulating a json string")
     return ComputeTotalDisplacementProcess(Model, settings["Parameters"])
 
-def TotalDisplacement(node, initial_strain):
-    # compute regular displacements
-    comp_x =  initial_strain[0] * node.X0 + 0.5 * initial_strain[3] * node.Y0 + 0.5 * initial_strain[5] * node.Z0
-    comp_y = 0.5 * initial_strain[3] * node.X0 + initial_strain[1] * node.Y0 + initial_strain[4] * node.Z0
-    comp_z = 0.5 * initial_strain[5] * node.X0 + 0.5 * initial_strain[4] * node.Y0 + initial_strain[3] * node.Z0
-
+def TotalDisplacement(node, strain):
+    s_xx = strain[0]
+    s_yy = strain[1]
+    s_zz = strain[2]
+    s_xy = 0.5 * strain[3]
+    s_yz = 0.5 * strain[4]
+    s_xz = 0.5 * strain[5]
+    comp_x = s_xx * node.X0 + s_xy * node.Y0 + s_xz * node.Z0
+    comp_y = s_xy * node.X0 + s_yy * node.Y0 + s_yz * node.Z0
+    comp_z = s_xz * node.X0 + s_yz * node.Y0 + s_zz * node.Z0
     displ = node.GetSolutionStepValue(km.DISPLACEMENT)
-    total_dis_X = comp_x + displ[0]
-    total_dis_Y = comp_y + displ[1]
-    total_dis_Z = comp_z + displ[2]
-
-    # Total displacement
-    total_displ = [node.Id, total_dis_X, total_dis_Y, total_dis_Z]
-
-    return total_displ
+    total_displ_X = comp_x + displ[0]
+    total_displ_Y = comp_y + displ[1]
+    total_displ_Z = comp_z + displ[2]
+    return [total_displ_X, total_displ_Y, total_displ_Z]
 
 
 class ComputeTotalDisplacementProcess(km.Process):
@@ -34,56 +34,60 @@ class ComputeTotalDisplacementProcess(km.Process):
         {
             "mesh_id": 0,
             "model_part_name": "unset_model_part_name",
-            "filename": "unset_filename",
-            "write_mode": "ascii"
         }
         """)
+        #"filename": "unset_filename",
+        #"write_mode": "ascii"
         settings.ValidateAndAssignDefaults(default_settings)
         self.model_part = Model[settings["model_part_name"].GetString()]
-        self.filename = settings['filename'].GetString()
-        self.write_mode = settings['write_mode'].GetString()
+        #self.filename = settings['filename'].GetString()
+        #self.write_mode = settings['write_mode'].GetString()
 
-    def write_results(self, filename):
-        def write_results_binary():
-            with open(filename, 'wb') as ofile:
-                initial_strain = self.model_part.ProcessInfo[km.INITIAL_STRAIN]
-                for node in self.model_part.Nodes:
-                    nodal_displ = TotalDisplacement(node, initial_strain)
-                    #for v in total_displ:
-                    for v in nodal_displ:
-                        ofile.write(struct.pack('f', v)) # 'f'=float32
-                    ofile.write(b'\n')
+    #def write_results(self, filename):
+    #    def write_results_binary():
+    #        with open(filename, 'wb') as ofile:
+    #            initial_strain = self.model_part.ProcessInfo[km.INITIAL_STRAIN]
+    #            for node in self.model_part.Nodes:
+    #                nodal_displ = TotalDisplacement(node, initial_strain)
+    #                #for v in total_displ:
+    #                for v in nodal_displ:
+    #                    ofile.write(struct.pack('f', v)) # 'f'=float32
+    #                ofile.write(b'\n')
 
-        def write_results_ascii():
-            with open(filename, 'w') as ofile:
-                initial_strain = self.model_part.ProcessInfo[km.INITIAL_STRAIN]
-                for node in self.model_part.Nodes:
-                    nodal_displ = TotalDisplacement(node, initial_strain)
-                    for v in nodal_displ:
-                        ofile.write("{:18.16f} ".format(v))
-                    ofile.write("\n")
+    #    def write_results_ascii():
+    #        with open(filename, 'w') as ofile:
+    #            initial_strain = self.model_part.ProcessInfo[km.INITIAL_STRAIN]
+    #            for node in self.model_part.Nodes:
+    #                nodal_displ = TotalDisplacement(node, initial_strain)
+    #                for v in nodal_displ:
+    #                    ofile.write("{:18.16f} ".format(v))
+    #                ofile.write("\n")
 
-        if self.write_mode == "binary":
-            write_results_binary()
-        else:
-            write_results_ascii()
+    #    if self.write_mode == "binary":
+    #        write_results_binary()
+    #    else:
+    #        write_results_ascii()
 
-    def ExecuteInitialize(self):
-        try:
-            os.remove(self.filename)
-        except OSError:
-            pass
+    #def ExecuteInitialize(self):
+    #    try:
+    #        os.remove(self.filename)
+    #    except OSError:
+    #        pass
 
-    def ExecuteInitializeSolutionStep(self):
-        self.timestep = "-{:.3f}".format(self.model_part.ProcessInfo[km.TIME])
-        try:
-            os.remove(self.filename + self.timestep)
-        except OSError:
-            pass
-        #pass
+    #def ExecuteInitializeSolutionStep(self):
+    #    #self.timestep = "-{:.3f}".format(self.model_part.ProcessInfo[km.TIME])
+    #    #try:
+    #    #    os.remove(self.filename + self.timestep)
+    #    #except OSError:
+    #    #    pass
+    #    pass
 
     def ExecuteFinalizeSolutionStep(self):
-        self.write_results(self.filename + self.timestep)
+        #self.write_results(self.filename + self.timestep)
+        initial_strain = self.model_part.ProcessInfo[km.INITIAL_STRAIN]
+        for node in self.model_part.Nodes:
+            total_displ = TotalDisplacement(node, initial_strain)
+            node.SetSolutionStepValue(km.LAGRANGE_DISPLACEMENT, 0, total_displ)
 
-    def ExecuteFinalize(self):
-        pass
+    #def ExecuteFinalize(self):
+    #    pass
