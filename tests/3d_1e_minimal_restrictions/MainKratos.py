ebcd584245c1695247e389a2c386e55280a7c26b
Marcelo <marcelo.raschi@gmail.com>
2018-06-05 15:22:19 +0200

    update example to new analysis stage schema

--- tests/3d_1e_minimal_restrictions/MainKratos.py
+++ tests/3d_1e_minimal_restrictions/MainKratos.py
@@ -1,161 +1,252 @@
-# makes KratosMultiphysics backward compatible with python 2.6 and 2.7
-from __future__ import print_function, absolute_import, division
+from __future__ import print_function, absolute_import, division  # makes KratosMultiphysics backward compatible with python 2.6 and 2.7
 
-import time as timer
-#import operator
-import KratosMultiphysics as km
-import KratosMultiphysics.StructuralMechanicsApplication
+# Importing Kratos
+import KratosMultiphysics
+import KratosMultiphysics.StructuralMechanicsApplication as StructuralMechanicsApplication
 import KratosMultiphysics.MultiscaleROMApplication as msr
-import KratosMultiphysics.ExternalSolversApplication
-km.CheckForPreviousImport()
+
+# Importing the solvers (if available)
+try:
+    import KratosMultiphysics.ExternalSolversApplication
+    KratosMultiphysics.Logger.PrintInfo("ExternalSolversApplication", "succesfully imported")
+except ImportError:
+    KratosMultiphysics.Logger.PrintInfo("ExternalSolversApplication", "not imported")
+try:
+    import KratosMultiphysics.EigenSolversApplication
+    KratosMultiphysics.Logger.PrintInfo("EigenSolversApplication", "succesfully imported")
+except ImportError:
+    KratosMultiphysics.Logger.PrintInfo("EigenSolversApplication", "not imported")
+
+# Importing the base class
+from analysis_stage import AnalysisStage
+
+# Other imports
+import sys
+
+class StructuralMechanicsAnalysis(AnalysisStage):
+    """
+    This class is the main-script of the StructuralMechanicsApplication put in a class
+
+    It can be imported and used as "black-box"
+    """
+    def __init__(self, model, project_parameters):
+        super(StructuralMechanicsAnalysis, self).__init__(model, project_parameters)
+
+        solver_settings = project_parameters["solver_settings"]
+        if not solver_settings.Has("time_stepping"):
+            KratosMultiphysics.Logger.PrintInfo("StructuralMechanicsAnalysis", "Using the old way to pass the time_step, this will be removed!")
+            time_stepping_params = KratosMultiphysics.Parameters("{}")
+            time_stepping_params.AddValue("time_step", project_parameters["problem_data"]["time_step"])
+            solver_settings.AddValue("time_stepping", time_stepping_params)
+
+        if not solver_settings.Has("domain_size"):
+            KratosMultiphysics.Logger.PrintInfo("StructuralMechanicsAnalysis", "Using the old way to pass the domain_size, this will be removed!")
+            solver_settings.AddEmptyValue("domain_size")
+            solver_settings["domain_size"].SetInt(project_parameters["problem_data"]["domain_size"].GetInt())
+
+        if not solver_settings.Has("model_part_name"):
+            KratosMultiphysics.Logger.PrintInfo("StructuralMechanicsAnalysis", "Using the old way to pass the model_part_name, this will be removed!")
+            solver_settings.AddEmptyValue("model_part_name")
+            solver_settings["model_part_name"].SetString(project_parameters["problem_data"]["model_part_name"].GetString())
+
+        ## Get echo level and parallel type
+        self.echo_level = self.project_parameters["problem_data"]["echo_level"].GetInt()
+        self.parallel_type = self.project_parameters["problem_data"]["parallel_type"].GetString()
+
+        ## Import parallel modules if needed
+        if (self.parallel_type == "MPI"):
+            import KratosMultiphysics.mpi as KratosMPI
+            import KratosMultiphysics.MetisApplication as MetisApplication
+            import KratosMultiphysics.TrilinosApplication as TrilinosApplication
+            self.is_printing_rank = (KratosMPI.mpi.rank == 0)
+        else:
+            self.is_printing_rank = True
+
+        self._CreateSolver()
+
+    def Initialize(self):
+        self.ModifyInitialProperties()
+        self.ModifyInitialGeometry()
+        self._ExecuteInitialize()
+        self._SetUpListOfProcesses()
+        ## Processes initialization
+        for process in self.list_of_processes:
+            process.ExecuteInitialize()
+
+        ## Solver initialization
+        self.solver.Initialize()
+
+        self._ExecuteBeforeSolutionLoop()
+
+    def InitializeSolutionStep(self):
+        super(StructuralMechanicsAnalysis, self).InitializeSolutionStep()
+
+        if self.is_printing_rank:
+            KratosMultiphysics.Logger.PrintInfo(self._GetSimulationName(), "STEP: ", self.solver.GetComputingModelPart().ProcessInfo[KratosMultiphysics.STEP])
+            KratosMultiphysics.Logger.PrintInfo(self._GetSimulationName(), "TIME: ", self.time)
+        sys.stdout.flush()
+
+    def OutputSolutionStep(self):
+        if self.have_output and self.output.IsOutputStep():
+
+            for process in self.list_of_processes:
+                process.ExecuteBeforeOutputStep()
+
+            self.output.PrintOutput()
+
+            for process in self.list_of_processes:
+                process.ExecuteAfterOutputStep()
+
+        self.solver.SaveRestart() # whether a restart-file is written is decided internally
+
+    def Finalize(self):
+        super(StructuralMechanicsAnalysis, self).Finalize()
+
+        if self.is_printing_rank:
+            KratosMultiphysics.Logger.PrintInfo(self._GetSimulationName(), "Analysis -END- ")
 
 
-def ApplyLoad(initial_value, model_part, time):
-    for node in model_part.Nodes:
-        factor = 1.
-        value = initial_value * factor * time
-        node.SetSolutionStepValue(km.POINT_LOAD_Y, 0, value)
+    #### Internal functions ####
+    def _CreateSolver(self):
+        """ Create the Solver (and create and import the ModelPart if it is not alread in the model) """
+        ## Solver construction
+        import python_solvers_wrapper_structural
+        self.solver = python_solvers_wrapper_structural.CreateSolver(self.model, self.project_parameters)
+
+        ## Adds the necessary variables to the model_part only if they don't exist
+        self.solver.AddVariables()
+
+        self.solver.ImportModelPart() # TODO move to global instance
+
+    def _SetUpGiDOutput(self):
+        '''Initialize self.output as a GiD output instance.'''
+        self.have_output = self.project_parameters.Has("output_configuration")
+        if self.have_output:
+            self.__CheckForDeprecatedGiDSettings()
+            if self.parallel_type == "OpenMP":
+                from gid_output_process import GiDOutputProcess as OutputProcess
+            elif self.parallel_type == "MPI":
+                from gid_output_process_mpi import GiDOutputProcessMPI as OutputProcess
+
+            self.output = OutputProcess(self.solver.GetComputingModelPart(),
+                                        self.project_parameters["problem_data"]["problem_name"].GetString() ,
+                                        self.project_parameters["output_configuration"])
+
+    def _ExecuteInitialize(self):
+        """ Initializing the Analysis """
+        ## ModelPart is being prepared to be used by the solver
+        self.solver.PrepareModelPart()
+
+        ## Adds the Dofs if they don't exist
+        self.solver.AddDofs()
+
+        main_model_part = self.model.GetModelPart(self.project_parameters["solver_settings"]["model_part_name"].GetString())
+        lmutility = msr.LagrangeMultiplierUtility(main_model_part)
+        lmutility.Execute()
+        main_model_part.Nodes[1].AddDof(msr.LAGRANGE_MULTIPLIER_1)
+        main_model_part.Nodes[1].AddDof(msr.LAGRANGE_MULTIPLIER_2)
+        main_model_part.Nodes[1].AddDof(msr.LAGRANGE_MULTIPLIER_3)
+        main_model_part.Nodes[1].AddDof(msr.LAGRANGE_MULTIPLIER_4)
+        main_model_part.Nodes[1].AddDof(msr.LAGRANGE_MULTIPLIER_5)
+        main_model_part.Nodes[1].AddDof(msr.LAGRANGE_MULTIPLIER_6)
+
+        parameter_string = '''{
+        "name_auxiliar_model_part" : "SKIN",
+        "name_auxiliar_condition"  : "MinimalKineticCondition",
+        "echo_level"               : 1
+        }'''
+        detect_skin = KratosMultiphysics.SkinDetectionProcess3D(
+            self.solver.GetComputingModelPart(),
+            KratosMultiphysics.Parameters(parameter_string))
+        detect_skin.Execute()
 
 
-########################
-# main
-########################
+    def _SetUpListOfProcesses(self):
+        from process_factory import KratosProcessFactory
+        factory = KratosProcessFactory(self.model)
+        self.list_of_processes = factory.ConstructListOfProcesses(self.project_parameters["constraints_process_list"])
+        self.list_of_processes += factory.ConstructListOfProcesses(self.project_parameters["loads_process_list"])
+        if (self.project_parameters.Has("list_other_processes") is True):
+            self.list_of_processes += factory.ConstructListOfProcesses(self.project_parameters["list_other_processes"])
+        if (self.project_parameters.Has("json_output_process") is True):
+            self.list_of_processes += factory.ConstructListOfProcesses(self.project_parameters["json_output_process"])
+        # Processes for tests
+        if (self.project_parameters.Has("json_check_process") is True):
+            self.list_of_processes += factory.ConstructListOfProcesses(self.project_parameters["json_check_process"])
+        if (self.project_parameters.Has("check_analytic_results_process") is True):
+            self.list_of_processes += factory.ConstructListOfProcesses(self.project_parameters["check_analytic_results_process"])
 
-ProjectParameters = km.Parameters(open("ProjectParameters.json", 'r').read())
-echo_level = ProjectParameters["problem_data"]["echo_level"].GetInt()
-parallel_type = ProjectParameters["problem_data"]["parallel_type"].GetString()
+        #TODO this should be generic
+        # initialize GiD  I/O
+        self._SetUpGiDOutput()
+        if self.have_output:
+            self.list_of_processes += [self.output,]
 
-## Modelpart definition
-main_model_part = km.ModelPart(ProjectParameters["problem_data"]["model_part_name"].GetString())
-main_model_part.ProcessInfo.SetValue(km.DOMAIN_SIZE, ProjectParameters["problem_data"]["domain_size"].GetInt())
+        if self.is_printing_rank and self.echo_level > 1:
+            count = 0
+            for process in self.list_of_processes:
+                count += 1
+                # KratosMultiphysics.Logger.PrintInfo("Process " + str(count), process) # FIXME
 
-## Solver construction
-import python_solvers_wrapper_structural
-solver = python_solvers_wrapper_structural.CreateSolver(main_model_part, ProjectParameters)
-solver.AddVariables()
-main_model_part.AddNodalSolutionStepVariable(km.LAGRANGE_DISPLACEMENT)
-main_model_part.AddNodalSolutionStepVariable(msr.LAGRANGE_MULTIPLIER_1)
-main_model_part.AddNodalSolutionStepVariable(msr.LAGRANGE_MULTIPLIER_2)
-main_model_part.AddNodalSolutionStepVariable(msr.LAGRANGE_MULTIPLIER_3)
-main_model_part.AddNodalSolutionStepVariable(msr.LAGRANGE_MULTIPLIER_4)
-main_model_part.AddNodalSolutionStepVariable(msr.LAGRANGE_MULTIPLIER_5)
-main_model_part.AddNodalSolutionStepVariable(msr.LAGRANGE_MULTIPLIER_6)
-solver.ImportModelPart()
-solver.AddDofs()
-lmutility = msr.LagrangeMultiplierUtility(main_model_part)
-lmutility.Execute()
-main_model_part.Nodes[1].AddDof(msr.LAGRANGE_MULTIPLIER_1)
-main_model_part.Nodes[1].AddDof(msr.LAGRANGE_MULTIPLIER_2)
-main_model_part.Nodes[1].AddDof(msr.LAGRANGE_MULTIPLIER_3)
-main_model_part.Nodes[1].AddDof(msr.LAGRANGE_MULTIPLIER_4)
-main_model_part.Nodes[1].AddDof(msr.LAGRANGE_MULTIPLIER_5)
-main_model_part.Nodes[1].AddDof(msr.LAGRANGE_MULTIPLIER_6)
+    def _ExecuteBeforeSolutionLoop(self):
+        """ Perform Operations before the SolutionLoop """
 
-## Detection of skin and set of boundary conditions
-parameter_string = '''{
-"name_auxiliar_model_part" : "SKIN",
-"name_auxiliar_condition"  : "MinimalKineticCondition",
-"echo_level"               : 1
-}'''
-detect_skin = km.SkinDetectionProcess3D(solver.GetComputingModelPart(), km.Parameters(parameter_string))
-detect_skin.Execute()
+        for process in self.list_of_processes:
+            process.ExecuteBeforeSolutionLoop()
 
-## Initialize GiD I/O
-output_post = ProjectParameters.Has("output_configuration")
-if output_post:
-    from gid_output_process import GiDOutputProcess
-    gid_output = GiDOutputProcess(solver.GetComputingModelPart(),
-                                  ProjectParameters["problem_data"]["problem_name"].GetString(),
-                                  ProjectParameters["output_configuration"])
-    gid_output.ExecuteInitialize()
+        ## Writing the full ProjectParameters file before solving
+        if self.is_printing_rank and self.echo_level > 1:
+            f = open("ProjectParametersOutput.json", 'w')
+            f.write(self.project_parameters.PrettyPrintJsonString())
+            f.close()
 
-## Creation of the Kratos model (build sub_model_parts or submeshes)
-StructureModel = {ProjectParameters["problem_data"]["model_part_name"].GetString(): main_model_part}
+        ## Stepping and time settings
+        start_time = self.project_parameters["problem_data"]["start_time"].GetDouble()
+        self.end_time = self.project_parameters["problem_data"]["end_time"].GetDouble()
 
-## Get the list of the sub_model_parts in where the processes are to be applied
-for i in range(ProjectParameters["solver_settings"]["processes_sub_model_part_list"].size()):
-    part_name = ProjectParameters["solver_settings"]["processes_sub_model_part_list"][i].GetString()
-    StructureModel.update({part_name: main_model_part.GetSubModelPart(part_name)})
+        if self.solver.GetComputingModelPart().ProcessInfo[KratosMultiphysics.IS_RESTARTED] is True:
+            self.time = self.solver.GetComputingModelPart().ProcessInfo[KratosMultiphysics.TIME]
+        else:
+            self.time = start_time
 
-## Print model_part and properties
-if ((parallel_type == "OpenMP") or (mpi.rank == 0)) and (echo_level > 1):
-    print("")
-    print(main_model_part)
-    for properties in main_model_part.Properties:
-        print(properties)
+        if self.is_printing_rank:
+            KratosMultiphysics.Logger.PrintInfo(self._GetSimulationName(), "Analysis -START- ")
 
-## Processes construction
-import process_factory
-list_of_processes = process_factory.KratosProcessFactory(StructureModel)\
-    .ConstructListOfProcesses(ProjectParameters["constraints_process_list"])
-list_of_processes += process_factory.KratosProcessFactory(StructureModel)\
-    .ConstructListOfProcesses(ProjectParameters["loads_process_list"])
-if ((parallel_type == "OpenMP") or (mpi.rank == 0)) and (echo_level > 1):
-    for process in list_of_processes:
-        print(process)
+    def _GetSimulationName(self):
+        return "::[KSM Simulation]:: "
 
-## Processes initialization
-for process in list_of_processes:
-    process.ExecuteInitialize()
+    def __CheckForDeprecatedGiDSettings(self):
+        if self.project_parameters["output_configuration"].Has("result_file_configuration"):
+            res_file_config = self.project_parameters["output_configuration"]["result_file_configuration"]
+            if res_file_config.Has("nodal_results"):
+                nodal_res = res_file_config["nodal_results"]
+                for i in range(nodal_res.size()):
+                    var_name = nodal_res[i].GetString()
+                    if var_name == "TORQUE":
+                        err_msg  = 'Requesting output for "TORQUE" which is not available any more\n'
+                        err_msg += 'It was renamed to "REACTION_MOMENT"'
+                        raise Exception(err_msg)
 
-## Solver initialization
-solver.Initialize()
-solver.SetEchoLevel(echo_level)
+if __name__ == "__main__":
+    from sys import argv
 
-if (output_post == True):
-    gid_output.ExecuteBeforeSolutionLoop()
+    if len(argv) > 2:
+        err_msg =  'Too many input arguments!\n'
+        err_msg += 'Use this script in the following way:\n'
+        err_msg += '- With default ProjectParameters (read from "ProjectParameters.json"):\n'
+        err_msg += '    "python3 structural_mechanics_analysis.py"\n'
+        err_msg += '- With custom ProjectParameters:\n'
+        err_msg += '    "python3 structural_mechanics_analysis.py CustomProjectParameters.json"\n'
+        raise Exception(err_msg)
 
-for process in list_of_processes:
-    process.ExecuteBeforeSolutionLoop()
+    if len(argv) == 2: # ProjectParameters is being passed from outside
+        project_parameters_file_name = argv[1]
+    else: # using default name
+        project_parameters_file_name = "ProjectParameters.json"
 
-t0p = timer.clock()
-t0w = timer.time()
-## Temporal loop
-delta_time = ProjectParameters["problem_data"]["time_step"].GetDouble()
-start_time = ProjectParameters["problem_data"]["start_time"].GetDouble()
-end_time = ProjectParameters["problem_data"]["end_time"].GetDouble()
-time = start_time
-main_model_part.ProcessInfo[km.TIME_STEPS] = 0
+    with open(project_parameters_file_name,'r') as parameter_file:
+        parameters = KratosMultiphysics.Parameters(parameter_file.read())
 
-while(time <= end_time - delta_time):
-    time += delta_time
-    main_model_part.ProcessInfo[km.TIME_STEPS] += 1
-    main_model_part.CloneTimeStep(time)
-    if (parallel_type == "OpenMP") or (mpi.rank == 0):
-        print("")
-        print("STEP = ", main_model_part.ProcessInfo[km.TIME_STEPS])
-        print("TIME = ", time)
-
-    for process in list_of_processes:
-        process.ExecuteInitializeSolutionStep()
-    if output_post:
-        gid_output.ExecuteInitializeSolutionStep()
-
-    #ApplyLoad(-5250000, main_model_part.GetSubModelPart("LOAD"), time)
-    solver.Solve()
-
-    for process in list_of_processes:
-        process.ExecuteFinalizeSolutionStep()
-    if output_post:
-        gid_output.ExecuteFinalizeSolutionStep()
-
-    for process in list_of_processes:
-        process.ExecuteBeforeOutputStep()
-    if output_post and gid_output.IsOutputStep():
-        gid_output.PrintOutput()
-
-    for process in list_of_processes:
-        process.ExecuteAfterOutputStep()
-
-
-for process in list_of_processes:
-    process.ExecuteFinalize()
-if output_post:
-    gid_output.ExecuteFinalize()
-
-tfp = timer.clock()
-tfw = timer.time()
-print("")
-print("Computing Time = {:.2f} s ({:.2f} s wall-time)".format(tfp - t0p, tfw - t0w))
-print(timer.ctime())
-
+    model = KratosMultiphysics.Model()
+    simulation = StructuralMechanicsAnalysis(model, parameters)
+    simulation.Run()
