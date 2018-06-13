5c40973a95064127877c44b44d7f0e58d0e87680
Marcelo <marcelo.raschi@gmail.com>
2018-06-13 12:42:27 +0200

    update multiscale test case (WIP)

---
+++ tests/multiscale/MainKratos.py
@@ -0,0 +1,23 @@
+# makes KratosMultiphysics backward compatible with python 2.6 and 2.7
+from __future__ import print_function, absolute_import, division 
+
+import KratosMultiphysics
+import KratosMultiphysics.StructuralMechanicsApplication as StructuralMechanics
+import KratosMultiphysics.MultiscaleROMApplication
+
+from structural_mechanics_analysis import StructuralMechanicsAnalysis
+
+class StructuralMechanicsAnalysisMultiscale(StructuralMechanicsAnalysis):
+    """This class prints information abt the computen Eigenvectors
+    It also shows how the "StructuralMechanicsAnalysis" can be customized by deriving from it
+    """
+    pass
+
+if __name__ == "__main__":
+
+    with open("ProjectParameters.json",'r') as parameter_file:
+        parameters = KratosMultiphysics.Parameters(parameter_file.read())
+
+    model = KratosMultiphysics.Model()
+    simulation = StructuralMechanicsAnalysisMultiscale(model,parameters)
+    simulation.Run()
