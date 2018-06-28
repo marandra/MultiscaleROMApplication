c0fdf3205cb10bd74f720eafb53c320d803a442f
Marcelo <marcelo.raschi@gmail.com>
2018-06-28 13:18:14 +0200

    update multiscale test case

--- tests/multiscale/micro1/micro_MainKratos.py
+++ tests/multiscale/micro1/micro_MainKratos.py
@@ -4,7 +4,14 @@ from __future__ import print_function, absolute_import, division
 import KratosMultiphysics
 import KratosMultiphysics.MultiscaleROMApplication
 import KratosMultiphysics.StructuralMechanicsApplication
+
 from structural_mechanics_analysis import StructuralMechanicsAnalysis
+
+#class StructuralMechanicsAnalysisMultiscale(StructuralMechanicsAnalysis):
+#    """This class prints information abt the computen Eigenvectors
+#    It also shows how the "StructuralMechanicsAnalysis" can be customized by deriving from it
+#    """
+#    pass
 
 if __name__ == "__main__":
 
@@ -12,5 +19,6 @@ if __name__ == "__main__":
         parameters = KratosMultiphysics.Parameters(parameter_file.read())
 
     model = KratosMultiphysics.Model()
+    #simulation = StructuralMechanicsAnalysisMultiscale(model,parameters)
     simulation = StructuralMechanicsAnalysis(model,parameters)
     simulation.Run()
