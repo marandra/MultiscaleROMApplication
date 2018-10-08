e8aba732e6467d44d10b8c325a19296788dcab32
Marcelo <marcelo.raschi@gmail.com>
2018-10-08 14:59:56 +0200

    Test for RVE Law. Auxiliary test cases

--- reduction-stage/test_rve.py
+++ reduction-stage/test_rve.py
@@ -1,12 +1,8 @@
 from __future__ import print_function, absolute_import, division
 import KratosMultiphysics
 import KratosMultiphysics.StructuralMechanicsApplication as StructuralMechanicsApplication
-import KratosMultiphysics.MultiscaleROMApplication as kmsr
-import configparser
-
-import KratosMultiphysics.KratosUnittest as KratosUnittest
-
-import math
+import KratosMultiphysics.MultiscaleROMApplication as MultiscaleROMApplication
+import os
 
 def _create_geometry(model_part, dim):
     # Create new nodes
@@ -95,35 +91,47 @@ def _set_cl_options(dict_options):
     return cl_options
 
 
-def _print_cl_output(cl, cl_params, properties, geom, N, model_part):
-    print("The Material Response PK2")
-    cl.CalculateMaterialResponsePK2(cl_params)
-    print("Stress = ", cl_params.GetStressVector())
-    print("Strain = ", cl_params.GetStrainVector())
-    print("C      = ", cl_params.GetConstitutiveMatrix())
+class Output:
+    def __init__(self, filename):
+        try:
+            os.remove(filename)
+        except OSError:
+            pass
+        self.fo = open(filename, "w")
+        self.fo.write("#    {:<78}{:<30}\n".format("Strain", "Stress"))
+        self.fo.write("#    "
+                      "{:<12} {:<12} {:<12} {:<12} {:<12} {:<12} "
+                      "{:<12} {:<12} {:<12} {:<12} {:<12} {:<12}\n".format(
+            "XX", "YY", "ZZ", "XY" , "YZ", "XZ",
+            "XX", "YY", "ZZ", "XY" , "YZ", "XZ"))
 
-    cl.FinalizeMaterialResponsePK2(cl_params)
-    cl.FinalizeSolutionStep(properties, geom, N, model_part.ProcessInfo)
+    def write(self, i, cl_params):
+        line = "{:<4} "\
+               "{:<+1.4e}  {:<+1.4e}  {:<+1.4e}  {:<+1.4e}  {:<+1.4e}  {:<+1.4e}  "\
+               "{:<+1.4e}  {:<+1.4e}  {:<+1.4e}  {:<+1.4e}  {:<+1.4e}  {:<+1.4e}\n".format(
+            i,
+            cl_params.GetStrainVector()[0],
+            cl_params.GetStrainVector()[1],
+            cl_params.GetStrainVector()[2],
+            cl_params.GetStrainVector()[3],
+            cl_params.GetStrainVector()[4],
+            cl_params.GetStrainVector()[5],
+            cl_params.GetStressVector()[0],
+            cl_params.GetStressVector()[1],
+            cl_params.GetStressVector()[2],
+            cl_params.GetStressVector()[3],
+            cl_params.GetStressVector()[4],
+            cl_params.GetStressVector()[5]
+        )
+        self.fo.write(line)
 
-    print("\nThe Material Response Kirchhoff")
-    cl.CalculateMaterialResponseKirchhoff(cl_params)
-    print("Stress = ", cl_params.GetStressVector())
-    print("Strain = ", cl_params.GetStrainVector())
-    print("C      = ", cl_params.GetConstitutiveMatrix())
+    def print(self, i, cl_params):
+        print("\nStep: {}".format(i))
+        print("Strain = ", cl_params.GetStrainVector())
+        print("Stress = ", cl_params.GetStressVector())
+        print("C      = ", cl_params.GetConstitutiveMatrix())
 
-    cl.FinalizeMaterialResponseKirchhoff(cl_params)
-    cl.FinalizeSolutionStep(properties, geom, N, model_part.ProcessInfo)
-
-    print("\nThe Material Response Cauchy")
-    cl.CalculateMaterialResponseCauchy(cl_params)
-    print("Stress = ", cl_params.GetStressVector())
-    print("Strain = ", cl_params.GetStrainVector())
-    print("C      = ", cl_params.GetConstitutiveMatrix())
-
-    cl.FinalizeMaterialResponseCauchy(cl_params)
-    cl.FinalizeSolutionStep(properties, geom, N, model_part.ProcessInfo)
-
-def _generic_constitutive_law_test(model_part, deformation_test):
+def generic_constitutive_law_test(model_part, deformation_test):
     # Define geometry
     [geom, nnodes] = _create_geometry(model_part, deformation_test.cl.dim)
 
@@ -161,6 +169,7 @@ def _generic_constitutive_law_test(model_part, deformation_test):
     # Check the results
     deformation_test.initialize_reference_stress(cl.GetStrainSize())
 
+    output = Output("strain-stress.dat")
     for i in range(deformation_test.nr_timesteps):
         deformation_test.set_deformation(cl_params, i)
 
@@ -168,16 +177,12 @@ def _generic_constitutive_law_test(model_part, deformation_test):
         cl.CalculateMaterialResponseCauchy(cl_params)
         cl.FinalizeMaterialResponseCauchy(cl_params)
         cl.FinalizeSolutionStep(properties, geom, N, model_part.ProcessInfo)
+
+        #output.print(i, cl_params)
+        output.write(i, cl_params)
+
         reference_stress = deformation_test.get_reference_stress(i)
-
         stress = cl_params.GetStressVector()
-        #DEBUG
-        print(stress)
-
-        #tolerance = 1.0e-4
-        #for j in range(cl.GetStrainSize()):
-        #    if (abs(stress[j]) > tolerance):
-        #        assertAlmostEqual((reference_stress[j] - stress[j])/stress[j], 0.0, msg=("Error checking solution " + str(stress[j]) + " different from " + str(reference_stress[j]) + " with tolerance of " + str(tolerance)), delta=tolerance)
 
 
 class LinearElastic():
@@ -216,14 +221,29 @@ class LinearJ2Plasticity3D(LinearJ2Plasticity):
         LinearJ2Plasticity.__init__(self)
         self.dim = 3
 
-    @staticmethod
-    def create_constitutive_Law():
+    #@staticmethod
+    def create_constitutive_Law(self):
         return StructuralMechanicsApplication.LinearJ2Plasticity3DLaw()
 
 
+class RVELaw():
+   def __init__(self, material_parameters):
+       self.dim = 3
+       self.material_parameters = material_parameters
+
+   def create_properties(self, model_part):
+       prop_id = 0
+       properties = model_part.Properties[prop_id]
+       return properties
+
+   def create_constitutive_Law(self):
+       return MultiscaleROMApplication.RVELaw().Create(self.material_parameters)
+
+
 class Deformation():
-    def __init__(self):
-        self.nr_timesteps = 100
+    def __init__(self, parameters):
+        self.nr_timesteps = parameters["nr_timesteps"]
+        self.strain_list = parameters["strain"]
 
     def get_init_deformation_gradientF(self):
         self.F = KratosMultiphysics.Matrix(self.cl.dim,self.cl.dim)
@@ -247,28 +267,23 @@ class Deformation():
         cl_params.SetDeterminantF(detF)
 
 
-class DeformationLinearJ2Plasticity(Deformation):
-    def __init__(self):
-        Deformation.__init__(self)
-        self.nr_timesteps = 10
+class DeformationLinearJ2Plasticity3D(Deformation):
+    def __init__(self, parameters):
+        Deformation.__init__(self, parameters)
+        self.cl = LinearJ2Plasticity3D()
 
     def set_deformation(self, cl_params, i):
         self.strain = (i+1)/ self.nr_timesteps * self.initial_strain
         cl_params.SetStrainVector(self.strain)
 
-class DeformationLinearJ2Plasticity3D(DeformationLinearJ2Plasticity):
-    def __init__(self):
-        DeformationLinearJ2Plasticity.__init__(self)
-        self.cl = LinearJ2Plasticity3D()
-
     def initialize_reference_stress(self, strain_size):
         self.initial_strain = KratosMultiphysics.Vector(strain_size)
-        self.initial_strain[0] = 0.001
-        self.initial_strain[1] = 0.001
-        self.initial_strain[2] = 0.0
-        self.initial_strain[3] = 0.001
-        self.initial_strain[4] = 0.0
-        self.initial_strain[5] = 0.001
+        self.initial_strain[0] = self.strain_list[0]
+        self.initial_strain[1] = self.strain_list[1]
+        self.initial_strain[2] = self.strain_list[2]
+        self.initial_strain[3] = self.strain_list[3]
+        self.initial_strain[4] = self.strain_list[4]
+        self.initial_strain[5] = self.strain_list[5]
 
         r_stress = []
         for i in range(self.nr_timesteps):
@@ -282,96 +297,72 @@ class DeformationLinearJ2Plasticity3D(DeformationLinearJ2Plasticity):
         r_stress[6][0] = 25.6595; r_stress[6][1] = 25.6595; r_stress[6][2] = 22.181 ; r_stress[6][3] = 1.73927; r_stress[6][4] = 0.0; r_stress[6][5] = 1.73927
         r_stress[7][0] = 29.1595; r_stress[7][1] = 29.1595; r_stress[7][2] = 25.681 ; r_stress[7][3] = 1.73928; r_stress[7][4] = 0.0; r_stress[7][5] = 1.73928
         r_stress[8][0] = 32.6595; r_stress[8][1] = 32.6595; r_stress[8][2] = 29.181 ; r_stress[8][3] = 1.73928; r_stress[8][4] = 0.0; r_stress[8][5] = 1.73928
-        r_stress[9][0] = 36.1595; r_stress[9][1] = 36.1595; r_stress[9][2] = 32.681; r_stress[9][3] = 1.73929; r_stress[9][4] = 0.0; r_stress[9][5] = 1.73929
+        r_stress[9][0] = 36.1595; r_stress[9][1] = 36.1595; r_stress[9][2] = 32.681 ; r_stress[9][3] = 1.73929; r_stress[9][4] = 0.0; r_stress[9][5] = 1.73929
+        self.reference_stress = r_stress
+
+    def get_reference_stress(self, i):
+        return self.reference_stress[i]
+
+
+class DeformationRVELaw(Deformation):
+    def __init__(self, parameters, material_parameters):
+        Deformation.__init__(self, parameters)
+        self.cl = RVELaw(material_parameters)
+
+    def set_deformation(self, cl_params, i):
+        self.strain = (i+1)/ self.nr_timesteps * self.initial_strain
+        cl_params.SetStrainVector(self.strain)
+
+    def initialize_reference_stress(self, strain_size):
+        self.initial_strain = KratosMultiphysics.Vector(strain_size)
+        self.initial_strain[0] = self.strain_list[0]
+        self.initial_strain[1] = self.strain_list[1]
+        self.initial_strain[2] = self.strain_list[2]
+        self.initial_strain[3] = self.strain_list[3]
+        self.initial_strain[4] = self.strain_list[4]
+        self.initial_strain[5] = self.strain_list[5]
+
+        r_stress = []
+        for i in range(self.nr_timesteps):
+            r_stress.append(KratosMultiphysics.Vector(strain_size))
+        r_stress[0][0] = 4.03846; r_stress[0][1] = 4.03846; r_stress[0][2] = 2.42308; r_stress[0][3] = 0.80769; r_stress[0][4] = 0.0; r_stress[0][5] = 0.80769
+        r_stress[1][0] = 8.07692; r_stress[1][1] = 8.07692; r_stress[1][2] = 4.84615; r_stress[1][3] = 1.61538; r_stress[1][4] = 0.0; r_stress[1][5] = 1.61538
+        r_stress[2][0] = 11.6595; r_stress[2][1] = 11.6595; r_stress[2][2] = 8.18099; r_stress[2][3] = 1.73926; r_stress[2][4] = 0.0; r_stress[2][5] = 1.73926
+        r_stress[3][0] = 15.1595; r_stress[3][1] = 15.1595; r_stress[3][2] = 11.681 ; r_stress[3][3] = 1.73926; r_stress[3][4] = 0.0; r_stress[3][5] = 1.73926
+        r_stress[4][0] = 18.6595; r_stress[4][1] = 18.6595; r_stress[4][2] = 15.181 ; r_stress[4][3] = 1.73926; r_stress[4][4] = 0.0; r_stress[4][5] = 1.73926
+        r_stress[5][0] = 22.1595; r_stress[5][1] = 22.1595; r_stress[5][2] = 18.681 ; r_stress[5][3] = 1.73927; r_stress[5][4] = 0.0; r_stress[5][5] = 1.73927
+        r_stress[6][0] = 25.6595; r_stress[6][1] = 25.6595; r_stress[6][2] = 22.181 ; r_stress[6][3] = 1.73927; r_stress[6][4] = 0.0; r_stress[6][5] = 1.73927
+        r_stress[7][0] = 29.1595; r_stress[7][1] = 29.1595; r_stress[7][2] = 25.681 ; r_stress[7][3] = 1.73928; r_stress[7][4] = 0.0; r_stress[7][5] = 1.73928
+        r_stress[8][0] = 32.6595; r_stress[8][1] = 32.6595; r_stress[8][2] = 29.181 ; r_stress[8][3] = 1.73928; r_stress[8][4] = 0.0; r_stress[8][5] = 1.73928
+        r_stress[9][0] = 36.1595; r_stress[9][1] = 36.1595; r_stress[9][2] = 32.681 ; r_stress[9][3] = 1.73929; r_stress[9][4] = 0.0; r_stress[9][5] = 1.73929
         self.reference_stress = r_stress
 
     def get_reference_stress(self, i):
         return self.reference_stress[i]
 
 #####################################################################
-
 if __name__ == "__main__":
-#    KratosUnittest.main()
 
-#    # parse configuration file
-#    conf = configparser.ConfigParser()
-#    conf.read("reduced_bases.cfg")
-#    nr_modes = int(conf['Parameters']['nr_active_modes'])
-#
-    # Define a model
+    parameters = {"nr_timesteps": 10,
+                 #[XX, YY, ZZ, XY, YZ, XZ]
+                  "strain": [0.001, 0.001, 0.0, 0.001, 0.0, 0.001]
+                  }
+
+    materials_rve  = KratosMultiphysics.Parameters("""
+    {
+        "name": "RVE Law",
+        "Parameters": {
+                "rve_materials_filename": "materials_rve.json",
+                "rve_data_filename": "rve.json"
+        }
+    }""")
+
     model_part = KratosMultiphysics.ModelPart("test")
 
-    deformation_test = DeformationLinearJ2Plasticity3D()
+    # Test plasticity
+    #deformation_test = DeformationLinearJ2Plasticity3D(parameters)
+    #generic_constitutive_law_test(model_part, deformation_test)
 
-    #self._generic_constitutive_law_test(model_part, deformation_test)
-    _generic_constitutive_law_test(model_part, deformation_test)
-#
-#
-#    model_part_rve = km.ModelPart("RVE")
-#    node1 = model_part_rve.CreateNewNode(1,0.0,0.0,0.0)
-#    geom = km.Triangle2D3(node1, node1, node1) # create point geom
-#    Model = {"RVE" : model_part_rve}
-#    materials_rve  = km.Parameters("""
-#               {
-#                   "Parameters": {
-#                           "materials_filename": "materials_rve.json"
-#                   }
-#           }
-#           """)
-#    read_materials_process.Factory(materials_rve, Model)
-#    #read_materials_process.ReadMaterialsProcess(Model, materials_rve)
-#
-#    # import rve_data json string
-#    with open ("rve.json", "r") as myfile:
-#        rve_data = km.Parameters(myfile.read())
-#
-#    cl = kmsr.RVELaw(model_part_rve, rve_data)
-#    cl_clone = cl.Clone()
-#
-#    cl.Check(km.ModelPart("dummy").Properties[1], geom, model_part_rve.ProcessInfo)
-#    cl.InitializeMaterial(km.ModelPart("dummy").Properties[1], geom, km.Vector(3))
-#
-#    nr_comp = cl.GetStrainSize()
-#    # creation and init
-#    init_strain_macro = km.Vector(nr_comp)
-#    homog_stress = km.Vector(nr_comp)
-#    homog_constit = km.Matrix(nr_comp, nr_comp)
-#    # trajectory 31:
-#    init_strain_macro[0] = 0.001
-#    init_strain_macro[1] = 0.0
-#    init_strain_macro[2] = 0.001
-#    init_strain_macro[3] = 0.0
-#    init_strain_macro[4] = 0.0
-#    init_strain_macro[5] = 0.001
-#
-#    cl_params = km.ConstitutiveLawParameters()
-#    cl_options = km.Flags()
-#    cl_options.Set(km.ConstitutiveLaw.COMPUTE_STRESS, True)
-#    cl_options.Set(km.ConstitutiveLaw.COMPUTE_CONSTITUTIVE_TENSOR, True)
-#    cl_params.SetOptions(cl_options)
-#    cl_params.SetStressVector(homog_stress)
-#    cl_params.SetConstitutiveMatrix(homog_constit)
-#    cl_params.SetMaterialProperties(model_part_rve.Properties[1])
-#
-#    nr_timesteps = 250
-#    t = dt = 1. / nr_timesteps
-#    fo = open("homog_stress.dat",'w')
-#    while (t <= 1. + dt / 10.):
-#        print("time {:.3f}".format(t))
-#        model_part_rve.CloneTimeStep(t)
-#        strain_macro = t * init_strain_macro
-#        cl_params.SetStrainVector(strain_macro)
-#        cl.CalculateMaterialResponseCauchy(cl_params)
-#        cl.FinalizeSolutionStep(km.ModelPart("dummy").Properties[1], geom,
-#                                km.Vector(3), model_part_rve.ProcessInfo)
-#        # Print output
-#        #modes_weights = km.Vector(nr_modes)
-#        #print(cl.GetValue(kmsr.REDUCED_MODES_WEIGHTS, modes_weights))
-#        cl_params.GetStressVector(homog_stress)
-#        #homog_stress = cl_params.GetStressVector()
-#        #print("{}: {}".format(t, homog_stress))
-#        #cl_params.GetConstitutiveMatrix(homog_constit)
-#        #print("{}: {}".format(t, homog_constit))
-#        t += dt
-#        fo.write("{}\n".format(homog_stress[0]))
-#    fo.close()
+    # Test RVE
+    deformation_test = DeformationRVELaw(parameters, materials_rve)
+    generic_constitutive_law_test(model_part, deformation_test)
