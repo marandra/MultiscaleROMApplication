013f9dadaa8f0ddeef66f42a2b3cf694ea9d885b
Marcelo <marcelo.raschi@gmail.com>
2018-11-12 13:44:53 +0100

    Removed unused builder and solvers form compilation

--- custom_python/add_custom_strategies_to_python.cpp
+++ custom_python/add_custom_strategies_to_python.cpp
@@ -16,8 +16,8 @@
 #include "linear_solvers/linear_solver.h"
 #include "spaces/ublas_space.h"
 
-#include "custom_strategies/builders_and_solvers/residualbased_rom_builder_and_solver.hpp"
-#include "custom_strategies/schemes/residualbased_incremental_rom_static_scheme.h"
+//#include "custom_strategies/builders_and_solvers/residualbased_rom_builder_and_solver.hpp"
+//#include "custom_strategies/schemes/residualbased_incremental_rom_static_scheme.h"
 
 namespace Kratos
 {
@@ -25,36 +25,36 @@ namespace Python
 {
 using namespace pybind11;
 
-typedef UblasSpace<double, CompressedMatrix, Vector> SparseSpaceType;
+//typedef UblasSpace<double, CompressedMatrix, Vector> SparseSpaceType;
 //typedef UblasSpace<double, Matrix, Vector> SparseSpaceType;
-typedef UblasSpace<double, Matrix, Vector> LocalSpaceType;
+//typedef UblasSpace<double, Matrix, Vector> LocalSpaceType;
 
 void  AddCustomStrategiesToPython(pybind11::module& m)
 {
     // base types
-    typedef LinearSolver<SparseSpaceType, LocalSpaceType> LinearSolverType;
-    typedef BuilderAndSolver<SparseSpaceType, LocalSpaceType, LinearSolverType> BuilderAndSolverType;
-    typedef Scheme<SparseSpaceType, LocalSpaceType> BaseSchemeType;
+    //typedef LinearSolver<SparseSpaceType, LocalSpaceType> LinearSolverType;
+    //typedef BuilderAndSolver<SparseSpaceType, LocalSpaceType, LinearSolverType> BuilderAndSolverType;
+    //typedef Scheme<SparseSpaceType, LocalSpaceType> BaseSchemeType;
 
     // custom builder_and_solver types
-    typedef ResidualBasedROMBuilderAndSolver<SparseSpaceType, LocalSpaceType, LinearSolverType> ResidualBasedROMBuilderAndSolverType;
-    typedef ResidualBasedIncrementalROMStaticScheme<SparseSpaceType, LocalSpaceType> ResidualBasedIncrementalROMStaticSchemeType;
+    //typedef ResidualBasedROMBuilderAndSolver<SparseSpaceType, LocalSpaceType, LinearSolverType> ResidualBasedROMBuilderAndSolverType;
+    //typedef ResidualBasedIncrementalROMStaticScheme<SparseSpaceType, LocalSpaceType> ResidualBasedIncrementalROMStaticSchemeType;
 
     //********************************************************************
     //*************************BUILDER AND SOLVER*************************
     //********************************************************************
 
-    class_<ResidualBasedROMBuilderAndSolverType, typename ResidualBasedROMBuilderAndSolverType::Pointer,
-            BuilderAndSolverType>, boost::noncopyable>(
-            "ResidualBasedROMBuilderAndSolver", init<LinearSolverType::Pointer>());
+    //class_<ResidualBasedROMBuilderAndSolverType, typename ResidualBasedROMBuilderAndSolverType::Pointer,
+    //        BuilderAndSolverType>, boost::noncopyable>(
+    //        "ResidualBasedROMBuilderAndSolver", init<LinearSolverType::Pointer>());
 
-    class_<ResidualBasedROMBuilderAndSolverType, bases<BuilderAndSolverType>, boost::noncopyable>(
-        "ResidualBasedROMBuilderAndSolver", init<LinearSolverType::Pointer>());
+    //class_<ResidualBasedROMBuilderAndSolverType, bases<BuilderAndSolverType>, boost::noncopyable>(
+    //    "ResidualBasedROMBuilderAndSolver", init<LinearSolverType::Pointer>());
 
-    class_<ResidualBasedIncrementalROMStaticSchemeType, bases<BaseSchemeType>, boost::noncopyable>(
-        "ResidualBasedIncrementalROMStaticScheme")
-        .def("Initialize",
-             &ResidualBasedIncrementalROMStaticScheme<SparseSpaceType, LocalSpaceType>::Initialize);
+    //class_<ResidualBasedIncrementalROMStaticSchemeType, bases<BaseSchemeType>, boost::noncopyable>(
+    //    "ResidualBasedIncrementalROMStaticScheme")
+    //    .def("Initialize",
+    //         &ResidualBasedIncrementalROMStaticScheme<SparseSpaceType, LocalSpaceType>::Initialize);
 }
 
 } // namespace Python.
