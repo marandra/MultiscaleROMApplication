8769717c7f3e4d7723f6bef7e7e31520163b61d5
Marcelo <marcelo.raschi@gmail.com>
2019-01-28 10:41:36 +0100

    Use CompressedMatrix for SparceSpaceType when exporting B&S

--- custom_python/add_custom_strategies_to_python.cpp
+++ custom_python/add_custom_strategies_to_python.cpp
@@ -17,7 +17,6 @@
 #include "spaces/ublas_space.h"
 
 #include "custom_strategies/builders_and_solvers/residualbased_block_builder_and_solver_custom.hpp"
-//#include "custom_strategies/schemes/residualbased_incremental_rom_static_scheme.h"
 
 namespace Kratos
 {
@@ -25,28 +24,28 @@ namespace Python
 {
 using namespace pybind11;
 
-//typedef UblasSpace<double, CompressedMatrix, Vector> SparseSpaceType;
-typedef UblasSpace<double, Matrix, Vector> SparseSpaceType;
+typedef UblasSpace<double, CompressedMatrix, Vector> SparseSpaceType;
 typedef UblasSpace<double, Matrix, Vector> LocalSpaceType;
 
 void  AddCustomStrategiesToPython(pybind11::module& m)
 {
     // base types
     typedef LinearSolver<SparseSpaceType, LocalSpaceType> LinearSolverType;
-    typedef BuilderAndSolver<SparseSpaceType, LocalSpaceType, LinearSolverType> BuilderAndSolverType;
+    //typedef BuilderAndSolver<SparseSpaceType, LocalSpaceType, LinearSolverType> BuilderAndSolverType;
+    typedef ResidualBasedBlockBuilderAndSolver<SparseSpaceType, LocalSpaceType, LinearSolverType> ResidualBasedBlockBuilderAndSolverType;
     //typedef Scheme<SparseSpaceType, LocalSpaceType> BaseSchemeType;
 
     // custom builder_and_solver types
     typedef ResidualBasedBlockBuilderAndSolverCustom<SparseSpaceType, LocalSpaceType, LinearSolverType> ResidualBasedBlockBuilderAndSolverCustomType;
-    //typedef ResidualBasedIncrementalROMStaticScheme<SparseSpaceType, LocalSpaceType> ResidualBasedIncrementalROMStaticSchemeType;
 
     //********************************************************************
     //*************************BUILDER AND SOLVER*************************
     //********************************************************************
 
-    pybind11::class_<ResidualBasedBlockBuilderAndSolverCustomType,
+   pybind11::class_<ResidualBasedBlockBuilderAndSolverCustomType,
         typename ResidualBasedBlockBuilderAndSolverCustomType::Pointer,
-        BuilderAndSolverType> (m, "ResidualBasedBlockBuilderAndSolverCustom")
+       //BuilderAndSolverType> (m, "ResidualBasedBlockBuilderAndSolverCustom")
+        ResidualBasedBlockBuilderAndSolverType> (m, "ResidualBasedBlockBuilderAndSolverCustom")
         .def(pybind11::init<LinearSolverType::Pointer>());
 }
 
