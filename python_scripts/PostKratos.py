b632fda2880c4b575dabfbdc4011ba6fe5ec46bf
Marcelo <marcelo.raschi@gmail.com>
2019-03-04 18:10:26 +0100

    WIP postprocessing scripts

---
+++ python_scripts/PostKratos.py
@@ -0,0 +1,40 @@
+#makes KratosMultiphysics backward compatible with python 2.6 and 2.7
+from __future__ import print_function, absolute_import, division
+
+import KratosMultiphysics as Kratos
+import KratosMultiphysics.StructuralMechanicsApplication
+import KratosMultiphysics.MultiscaleROMApplication
+from structural_mechanics_analysis import StructuralMechanicsAnalysis
+import compute_bases as bases
+
+"""
+For user-scripting it is intended that a new class is derived
+from StructuralMechanicsAnalysis to do modifications
+"""
+
+if __name__ == "__main__":
+
+    with open("PostProjectParameters.json",'r') as parameter_file:
+        parameters = KratosMultiphysics.Parameters(parameter_file.read())
+
+    model = KratosMultiphysics.Model()
+    simulation = StructuralMechanicsAnalysis(model,parameters)
+    simulation.Initialize()
+    rve_modelpart = simulation._GetSolver().GetComputingModelPart()
+    elem = rve_modelpart.GetElement(1)
+    ip_data = elem.GetValuesOnIntegrationPoints(Kratos.GREEN_LAGRANGE_STRAIN_VECTOR, rve_modelpart.ProcessInfo)
+
+    # gather model part info
+    nr_nodes = rve_modelpart.NumberOfNodes()
+    nr_elems = rve_modelpart.NumberOfElements()
+    nr_ips = len(elem.GetIntegrationPoints())
+    nr_strain_components = len(ip_data[0])
+    with open("integration_weight", 'w') as ofile:
+        for elem in rve_modelpart.Elements:
+            ip_weights = elem.GetValuesOnIntegrationPoints(Kratos.INTEGRATION_WEIGHT, rve_modelpart.ProcessInfo)
+            for ip_weight in ip_weights:
+                ofile.write("{}\n".format(ip_weight[0]))
+
+    # compute bases
+    bases.generate_bases_energy()
+    bases.generate_bases_strain()
