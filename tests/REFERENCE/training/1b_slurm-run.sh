7d9cc81e9bf6e9bc879d783a8c8e7695fb780c81
Marcelo <marcelo.raschi@gmail.com>
2019-05-30 11:08:09 +0200

    Updated slurm script

--- tests/REFERENCE/training/1b_slurm-run.sh
+++ tests/REFERENCE/training/1b_slurm-run.sh
@@ -2,22 +2,26 @@
 #SBATCH --job-name=fiber_array
 #SBATCH --ntasks-per-core=1
 #SBATCH --ntasks=1
-#SBATCH --partition=HM-dev
 #SBATCH --array=00-40
 
-##Optional - Required memory in MB per core. Defaults are 1GB per core.
-##SBATCH --mem-per-cpu=3072
+## Settings for "fiber2" case:
+##
+#SBATCH --partition=HM-dev
+#SBATCH --mem-per-cpu=1024
+#SBATCH --time=01:00:00
 
-##Optional - Estimated execution time
-##Acceptable time formats include  "minutes",   "minutes:seconds",
-##"hours:minutes:seconds",   "days-hours",   "days-hours:minutes" ,"days-hours:minutes:seconds".
-##SBATCH --time=
+## Settings for "fiber3" case:
+##
+##SBATCH --partition=HM
+##SBATCH --mem-per-cpu=????
+##SBATCH --time=????
+
 
 ########### Further details -> man sbatch ##########
 
 export OMP_NUM_THREADS=1
 printf -v ID "%02d\n" $SLURM_ARRAY_TASK_ID
-TRAJECTORYPATH=/comp-des-mat/mraschi/Cases/REFERENCE_2branches_damage/training/trajectory_$ID
+TRAJECTORYPATH=$PWD/trajectory_$ID
 cd $TRAJECTORYPATH
 time python3 MainKratos.py
 
