58ee3815bca92fecb13f033c9d564f4963bd7750
Marcelo <marcelo.raschi@gmail.com>
2020-01-09 17:56:30 +0100

    Clean up files

--- MultiscaleROMApplication.py
+++ MultiscaleROMApplication.py
@@ -1,8 +1,11 @@
+# makes KratosMultiphysics backward compatible with python 2.6 and 2.7
+from __future__ import print_function, absolute_import, division
+
+# Application dependent names and paths
 import KratosMultiphysics as KM
 from KratosMultiscaleROMApplication import *
 application = KratosMultiscaleROMApplication()
 application_name = "KratosMultiscaleROMApplication"
 application_folder = "MultiscaleROMApplication"
 
-# The following lines are common for all applications
 KM._ImportApplicationAsModule(application, application_name, application_folder, __path__)
