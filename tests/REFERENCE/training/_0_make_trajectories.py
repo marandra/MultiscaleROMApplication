601d25f0b1913f69dbf740f476c53e5e23d4ec6b
Marcelo <marcelo.raschi@gmail.com>
2020-01-31 14:44:43 +0100

    Create validation directory for cases skipping training

--- tests/REFERENCE/training/_0_make_trajectories.py
+++ tests/REFERENCE/training/_0_make_trajectories.py
@@ -1,4 +1,4 @@
-import  json
+import json
 from pathlib import Path, PurePath
 
 
@@ -7,7 +7,9 @@ def create_properties_file(props_fname, training_path, traj_name, case_path, str
     case_props_path = PurePath.joinpath(training_path, case_path, props_path)
     with props_path.open() as fi:
         props = json.load(fi)
-        props["processes"]["loads_process_list"][0]["Parameters"]["initial_strain"] = strain
+        props["processes"]["loads_process_list"][0]["Parameters"][
+            "initial_strain"
+        ] = strain
     with PurePath.joinpath(case_path, props_path).open("w") as fo:
         json.dump(props, fo, indent=4)
     return
@@ -24,11 +26,15 @@ def create_case_dir(traj_name, training_path, strain):
     case_path = Path("trajectory" + traj_name)
     case_path.mkdir(exist_ok=True)
     # populate case dir
-    create_properties_file("ProjectParameters.json", training_path, traj_name, case_path, strain)
+    create_properties_file(
+        "ProjectParameters.json", training_path, traj_name, case_path, strain
+    )
     copy_file("MainKratos.py", case_path)
     copy_file("model.mdpa", case_path)
     copy_file("materials.json", case_path)
     return
+
+
 ######################################
 # main
 ######################################
@@ -38,10 +44,12 @@ if __name__ == "__main__":
     with open(strain_set_fname, "r") as fi:
         strain_set = fi.readlines()
         nr_cases = len(strain_set)
-        nr_id = len(str(nr_cases - 1)) #  size of the case number string (e.g. nr_cases=100, id: 00..99, nr_id=2)
+        #  size of the case number string (e.g. nr_cases=100, id: 00..99, nr_id=2)
+        nr_id = len(str(nr_cases - 1))
         for i, line in enumerate(strain_set):
             traj_name = "_{:0{}d}".format(i, nr_id)
             strain = [float(x) for x in line.split()]
             print(traj_name, strain)
             create_case_dir(traj_name, Path("."), strain)
-
+    validation_path = Path("validation")
+    validation_path.mkdir(exist_ok=True)
