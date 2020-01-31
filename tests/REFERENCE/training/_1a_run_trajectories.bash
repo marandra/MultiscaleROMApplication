0fbf9f95e176e5aa01c550d9e462f2ae92911e87
Marcelo <marcelo.raschi@gmail.com>
2020-01-31 14:20:34 +0100

    Update trajectories running scripts

--- tests/REFERENCE/training/_1a_run_trajectories.bash
+++ tests/REFERENCE/training/_1a_run_trajectories.bash
@@ -4,8 +4,8 @@ for TRJ in trajectory_*
 do
         echo "Launching job "$TRJ
         echo "export OMP_NUM_THREADS=1" > tmp_$TRJ.bash
-        echo "export PYTHONPATH=${HOME}/Projects/Kratos/bin/current" >> tmp_$TRJ.bash
-        echo "export LD_LIBRARY_PATH=${HOME}/Projects/Kratos/bin/current/libs" >> tmp_$TRJ.bash
+        echo "export PYTHONPATH=${PYTHONPATH}" >> tmp_$TRJ.bash
+        echo "export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}" >> tmp_$TRJ.bash
         echo "cd "$TRJ >> tmp_$TRJ.bash
 	echo "time python3 MainKratos.py > outMainKratos 2>&1" >> tmp_$TRJ.bash
         echo "cd .." >> tmp_$TRJ.bash
@@ -15,5 +15,5 @@ done
 for TRJ in trajectory_*
 do
         bash tmp_$TRJ.bash
-        sleep 0.1
+        sleep 0.05
 done
