5a4223c1b1c5f78f4f425d22db8797d22fac2997
Marcelo <marcelo.raschi@gmail.com>
2020-02-14 16:07:30 +0100

    Offline process reads snapshots from hdf5

--- offline_scripts/offline_generate_rve_data.py
+++ offline_scripts/offline_generate_rve_data.py
@@ -1,17 +1,17 @@
-# makes KratosMultiphysics backward compatible with python 2.6 and 2.7
-from __future__ import print_function, absolute_import, division
 import os
 import numpy
 import KratosMultiphysics as Kratos
-import KratosMultiphysics.MultiscaleROMApplication
 from KratosMultiphysics.StructuralMechanicsApplication import (
     structural_mechanics_analysis,
 )
-import KratosMultiphysics.MultiscaleROMApplication.compute_bases as bases
-import KratosMultiphysics.MultiscaleROMApplication.compute_ip_weights as roc
-import KratosMultiphysics.MultiscaleROMApplication.pack_reduced_rve_dataset as pack
-import KratosMultiphysics.MultiscaleROMApplication.io_utilities as io_utilities
-import KratosMultiphysics.MultiscaleROMApplication.compute_stress_reconstruction_system as stress_reconstruction
+import KratosMultiphysics.MultiscaleROMApplication
+from KratosMultiphysics.MultiscaleROMApplication import (
+    compute_bases,
+    compute_ip_weights,
+    io_utilities,
+    pack_reduced_rve_dataset,
+)
+import h5py
 
 """
 TODO: pending description here.
@@ -36,6 +36,43 @@ def skip_calculation(filename, flag_reuse):
     return flag_exists and flag_reuse
 
 
+def create_bases(field_name, nr_elastic_modes, nr_inelastic_modes, trajectory_filename, svd_algorithm):
+    bases_fname = "bases_{}_{}m.npy".format(
+        field_name, nr_elastic_modes + nr_inelastic_modes
+    )
+    print("Generating {} bases".format(field_name))
+    e_files, i_files = compute_bases.list_of_snapshots(trajectory_filename, field_name)
+    compute_bases.generate_bases(
+        nr_elastic_modes,
+        nr_inelastic_modes,
+        e_files,
+        i_files,
+        bases_fname,
+        svd_algorithm=svd_algorithm,
+    )
+    try:
+        os.rename(
+            "singular_values_elastic.dat",
+            "sv_{}_elastic_{}.dat".format(field_name, nr_elastic_modes),
+        )
+        os.rename(
+            "singular_values_inelastic.dat",
+            "sv_{}_inelastic_{}.dat".format(field_name, nr_inelastic_modes),
+        )
+    except FileNotFoundError:
+        pass
+    try:
+        os.rename(
+            "singular_values.dat",
+            "sv_{}_{}.dat".format(field_name, nr_elastic_modes + nr_inelastic_modes),
+        )
+    except FileNotFoundError:
+        pass
+
+
+#######################################################################
+#######################################################################
+
 if __name__ == "__main__":
 
     with open("1_ProjectParameters.json", "r") as parameter_file:
@@ -48,11 +85,6 @@ if __name__ == "__main__":
     "svd_algorithm": "standard",
     "rve_materials_filename": "../training/materials.json",
     "trajectory_filename": "../training/trajectory",
-    "elastic_snapshots_filename": "elastic_timesteps",
-    "snapshot_energy_filename": "snapshot_energy",
-    "snapshot_strain_filename": "snapshot_strain",
-    "bases_energy_filename": "bases_energy",
-    "bases_strain_filename": "bases_strain",
     "nr_elastic_modes_energy": 21,
     "nr_inelastic_modes_energy": 200,
     "nr_elastic_modes_strain": 6,
@@ -74,15 +106,6 @@ if __name__ == "__main__":
     simulation.Initialize()
     rve_modelpart = simulation._GetSolver().GetComputingModelPart()
 
-    # Read the original model part used in training,
-    # and replaces elements and conditions needed by offline process
-    # Remove conditions from model part
-    #   for condition in rve_modelpart.Conditions:
-    #      condition.Set(KratosMultiphysics.TO_ERASE)
-    #   rve_modelpart.RemoveConditionsFromAllLevels(KratosMultiphysics.TO_ERASE)
-    #   print("DEBUG ********")
-    #   print(rve_modelpart)
-
     #
     # gather global model part info
     #
@@ -102,82 +125,50 @@ if __name__ == "__main__":
 
     svd_algorithm = config["svd_algorithm"].GetString()
     trajectory_filename = config["trajectory_filename"].GetString()
-    nr_e_snap_filename = config["elastic_snapshots_filename"].GetString()
+
     #
     # compute energy bases
     #
-    nr_strain_components = 1
+    field_name = "ENERGY_FREE"
     nr_elastic_modes = config["nr_elastic_modes_energy"].GetInt()
     nr_inelastic_modes = config["nr_inelastic_modes_energy"].GetInt()
-    energy_bases_fname = config[
-        "bases_energy_filename"
-    ].GetString() + "_{}m.npy".format(nr_elastic_modes + nr_inelastic_modes)
-    snapshot_filename = config["snapshot_energy_filename"].GetString()
-    print("Generating ENERGY bases")
-    if skip_calculation(energy_bases_fname, config["reuse_existing_files"].GetBool()):
-        print("File {} exists. Skipping calculation".format(energy_bases_fname))
+    bases_fname = "bases_{}_{}m.npy".format(
+        field_name, nr_elastic_modes + nr_inelastic_modes
+    )
+    energy_bases_fname = bases_fname
+    if not skip_calculation(bases_fname, config["reuse_existing_files"].GetBool()):
+        create_bases(field_name, nr_elastic_modes, nr_inelastic_modes, trajectory_filename, svd_algorithm)
     else:
-        e_files, i_files = bases.list_of_snapshots(
-            trajectory_filename, nr_e_snap_filename, snapshot_filename
-        )
-        bases.generate_bases(
-            nr_ips,
-            nr_strain_components,
-            nr_elastic_modes,
-            nr_inelastic_modes,
-            e_files,
-            i_files,
-            energy_bases_fname,
-            svd_algorithm=svd_algorithm,
-        )
-        #rename singular values file
-        try:
-            os.rename("singular_values_elastic.dat", "sv_energy_elastic_{}.dat".format(nr_elastic_modes))
-            os.rename("singular_values_inelastic.dat", "sv_energy_inelastic_{}.dat".format(nr_inelastic_modes))
-        except FileNotFoundError:
-            pass
-        try:
-            os.rename("singular_values.dat", "sv_energy_{}.dat".format(nr_elastic_modes+nr_inelastic_modes))
-        except FileNotFoundError:
-            pass
+        print("File {} exists. Skipping calculation".format(bases_fname))
 
     #
     # compute strain bases
     #
-    nr_strain_components = 6
+    field_name = "STRAIN_FLUCTUANT"
     nr_elastic_modes = config["nr_elastic_modes_strain"].GetInt()
     nr_inelastic_modes = config["nr_inelastic_modes_strain"].GetInt()
-    strain_bases_fname = config[
-        "bases_strain_filename"
-    ].GetString() + "_{}m.npy".format(nr_elastic_modes + nr_inelastic_modes)
-    snapshot_filename = config["snapshot_strain_filename"].GetString()
-    print("Generating STRAIN bases")
-    if skip_calculation(strain_bases_fname, config["reuse_existing_files"].GetBool()):
-        print("File {} exists. Skipping calculation".format(strain_bases_fname))
+    bases_fname = "bases_{}_{}m.npy".format(
+        field_name, nr_elastic_modes + nr_inelastic_modes
+    )
+    strain_bases_fname = bases_fname
+    if not skip_calculation(bases_fname, config["reuse_existing_files"].GetBool()):
+        create_bases(field_name, nr_elastic_modes, nr_inelastic_modes, trajectory_filename, svd_algorithm)
     else:
-        e_files, i_files = bases.list_of_snapshots(
-            trajectory_filename, nr_e_snap_filename, snapshot_filename
-        )
-        bases.generate_bases(
-            nr_ips,
-            nr_strain_components,
-            nr_elastic_modes,
-            nr_inelastic_modes,
-            e_files,
-            i_files,
-            strain_bases_fname,
-            svd_algorithm=svd_algorithm,
-        )
-        #rename singular values file
-        try:
-            os.rename("singular_values_elastic.dat", "sv_strain_elastic_{}.dat".format(nr_elastic_modes))
-            os.rename("singular_values_inelastic.dat", "sv_strain_inelastic_{}.dat".format(nr_inelastic_modes))
-        except FileNotFoundError:
-            pass
-        try:
-            os.rename("singular_values.dat", "sv_strain_{}.dat".format(nr_elastic_modes+nr_inelastic_modes))
-        except FileNotFoundError:
-            pass
+        print("File {} exists. Skipping calculation".format(bases_fname))
+
+    #
+    # compute DAMAGE bases
+    #
+    field_name = "DAMAGE"
+    nr_elastic_modes = 0
+    nr_inelastic_modes = 1400
+    bases_fname = "bases_{}_{}m.npy".format(
+        field_name, nr_elastic_modes + nr_inelastic_modes
+    )
+    if not skip_calculation(bases_fname, config["reuse_existing_files"].GetBool()):
+        create_bases(field_name, nr_elastic_modes, nr_inelastic_modes, trajectory_filename, svd_algorithm)
+    else:
+        print("File {} exists. Skipping calculation".format(bases_fname))
 
     #
     # compute ip set
@@ -195,11 +186,11 @@ if __name__ == "__main__":
         print("Generating {}".format(roc_filename))
         # compute ROC list
         if nr_roc_points != -1:  # HPROM case
-            roc_list = roc.compute_hprom_weights(
+            roc_list = compute_ip_weights.compute_hprom_weights(
                 ip_data, nr_roc_points, energy_bases_fname
             )
         else:  # ROM case
-            roc_list = roc.compute_rom_weights(ip_data)
+            roc_list = compute_ip_weights.compute_rom_weights(ip_data)
         with open(roc_filename, "w") as ofile:
             for list in roc_list:
                 ofile.write("{} {} {} {}\n".format(list[0], list[1], list[2], list[3]))
@@ -224,7 +215,7 @@ if __name__ == "__main__":
                 print("File {} exists. Skipping calculation".format(rve_data_filename))
                 continue
             print("Generating {}".format(rve_data_filename))
-            rve_params = pack.create_rve_params_structure(
+            rve_params = pack_reduced_rve_dataset.create_rve_params_structure(
                 strain_bases_fname,
                 rve_materials_filename,
                 nr_modes,
@@ -232,21 +223,3 @@ if __name__ == "__main__":
                 rve_modelpart,
             )
             io_utilities.write_json(rve_data_filename, rve_params)
-
-    # generate stress reconstruction system
-    # A = stress_reconstruction.compute_system(rve_data_filename, energy_bases_fname, integration_weights_filename)
-    # stress_reconstruction.util.write_numpy_file('correlation_stress_{}m_{}ip.npy'.format(nr_modes, nr_roc_points), 'binary', A)
-
-    # from multiscale_rom_analysis import StructuralMechanicsAnalysis
-    # settings = Kratos.Parameters("""
-    #    {
-    #        "element_name": "SmallDisplacementCustomElement3D8N",
-    #        "condition_name": ""
-    #    }
-    #    """)
-    # KratosMultiphysics.ReplaceElementsAndConditionsProcess(rve_modelpart, settings).Execute()
-    # print("DEBUG ********")
-    # print(rve_modelpart)
-
-    # generate strain-displacement correlation matrix
-    # simulation.Run()
--- python_scripts/compute_bases.py
+++ python_scripts/compute_bases.py
@@ -1,9 +1,9 @@
 import os
 import time
 import glob
-import numpy as np
-import scipy.sparse.linalg as sp
 import logging
+import h5py
+import numpy
 
 try:
     import sklearn.decomposition
@@ -17,44 +17,43 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-def list_of_snapshots(trajectory_filename, nr_e_snap_filename, filename):
+def list_of_snapshots(trajectory_filename, value_name):
     logger.debug("Scanning trajectories")
     trajectory_paths = sorted(glob.glob("{}_*".format(trajectory_filename)))
-    e_files = []
-    i_files = []
+    e_arrays = []
+    i_arrays = []
     for path in trajectory_paths:
-        with open("{}/{}".format(path, nr_e_snap_filename), "r") as f:
-            nr_e_snap = int(f.readline().strip())
-        logger.debug("  {} - elastic snapshots: {}".format(path, nr_e_snap))
-        e_files.extend(sorted(glob.glob("{}/{}*".format(path, filename)))[:nr_e_snap])
-        i_files.extend(sorted(glob.glob("{}/{}*".format(path, filename)))[nr_e_snap:])
-    logger.debug("")
-    return e_files, i_files
+        logger.debug("    trajectory {}".format(path))
+        with h5py.File(path + "/" + "snapshots.hdf5", "r") as f:
+            try:
+                d = f["ELASTIC"][value_name]
+                for k, v in d.items():
+                    e_arrays.append(numpy.array(v))
+            except KeyError:
+                pass
+            d = f["INELASTIC"][value_name]
+            for k, v in d.items():
+                i_arrays.append(numpy.array(v))
+    logger.debug("Nr of elastic arrays loaded: {}".format(len(e_arrays)))
+    logger.debug("Nr of inelastic arrays loaded: {}".format(len(i_arrays)))
+    return e_arrays, i_arrays
 
 
-def compute_modes(
-    nr_integration_points,
-    files,
-    nr_modes,
-    nr_components,
-    Ue=None,
-    svd_algorithm="standard",
-):
+def compute_modes(list_of_arrays, nr_modes, Ue=None, svd_algorithm="standard"):
     logger.info("Loading snapshots")
     if Ue is not None:
         logger.info("and removing elastic component")
-    nr_dofs = nr_integration_points * nr_components
-    X = np.empty([nr_dofs, len(files)])
-    total = len(files)
+    X = numpy.empty([len(list_of_arrays[0]), len(list_of_arrays)])
+    total = len(list_of_arrays)
     batch_size = int(total / 10 + 0.5)
     counter = 1
-    for i, file in enumerate(files):
-        X[:, i] = np.fromfile(file, dtype=np.float32)
+    for i, array in enumerate(list_of_arrays):
+        X[:, i] = array
         if Ue is not None:
             for j in range(Ue.shape[1]):
-                X[:, i] -= np.multiply(np.dot(Ue[:, j], X[:, i]), Ue[:, j])
+                X[:, i] -= numpy.multiply(numpy.dot(Ue[:, j], X[:, i]), Ue[:, j])
         if not counter % batch_size:
-            logger.debug("    {}/{} snapshots processed".format(counter, total))
+            logger.info("    {}/{} snapshots processed".format(counter, total))
         counter = counter + 1
     logger.info("")
 
@@ -70,96 +69,54 @@ def compute_modes(
         U = svd.components_.T
         S = svd.singular_values_.T
         logger.info("SVD time: {:.1f}s".format(time.time() - t0))
-    #    #elif svd_algorithm is "arpack":
-    #        t0 = time.time()
-    #        logger.info("Computing SVD using ARPACK algorithm")
-    #        svd = sklearn.decomposition.TruncatedSVD(n_components=nr_modes, algorithm="arpack")
-    #        svd.fit(X.T)
-    #        U = svd.components_.T
-    #        S = svd.singular_values_.T
-    #        print("DEBUG:")
-    #        print("SVD time: {:.1f}s".format(time.time() - t0))
-    #        print(svd)
-    #        #print(U)
-    #        #print(S)
-    #    if True:
-    #    #elif svd_algorithm is "iterative":
-    #        t0 = time.time()
-    #        logger.info("Computing SVD using ITERATIVE algorithm")
-    #        #[U, S] = sp.svds(X, k=nr_modes + 4)[:2]
-    #        [U, S] = sp.svds(X, k=nr_modes)[:2]
-    #        # to order values in decreasing order (svds returns them in increasing order)
-    #        S= S[::-1]
-    #        U = U[:,::-1]
-    #        U = U[:, :nr_modes]
-    #        print("DEBUG:")
-    #        print("SVD time: {:.1f}s".format(time.time() - t0))
-    #        #print(U)
-    #        #print(S)
     elif "standard" in svd_algorithm:
         t0 = time.time()
         logger.info("Computing SVD using STANDARD algorithm")
-        [U, S] = np.linalg.svd(X, full_matrices=False)[:2]
+        [U, S] = numpy.linalg.svd(X, full_matrices=False)[:2]
         U = U[:, :nr_modes]
         logger.info("SVD time: {:.1f}s".format(time.time() - t0))
 
     logger.info("    - SVD time: {:.1f}s".format(time.time() - t0))
     logger.info("    - singular value of selected modes:")
     logger.info("      {}".format(S[:nr_modes]))
-    #logger.info("      validation: following singular values (excluded):")
-    #logger.info("      {}".format(S[nr_modes: nr_modes + 4]))
+    logger.info("      validation: following singular values (excluded):")
+    logger.info("      {}".format(S[nr_modes : nr_modes + 4]))
     logger.info("    - nr and size of modes: {}, {}".format(U.shape[1], U.shape[0]))
     logger.info("")
-    np.savetxt("singular_values.dat", S)
+    numpy.savetxt("singular_values.dat", S)
 
     return U
 
 
 def generate_bases(
-    nr_ips,
-    nr_strain_components,
     nr_elastic_modes,
     nr_inelastic_modes,
-    e_files,
-    i_files,
+    e_arrays,
+    i_arrays,
     bases_fname,
     svd_algorithm="standard",
 ):
     t0 = time.time()
     if nr_elastic_modes > 0:
         logger.info("Processing elastic snapshots")
-        Ue = compute_modes(
-            nr_ips,
-            e_files,
-            nr_elastic_modes,
-            nr_components=nr_strain_components,
-            svd_algorithm=svd_algorithm,
-        )
+        Ue = compute_modes(e_arrays, nr_elastic_modes, svd_algorithm=svd_algorithm)
         os.rename("singular_values.dat", "singular_values_elastic.dat")
         logger.info("Processing inelastic snapshots")
+
         Ui = compute_modes(
-            nr_ips,
-            i_files,
-            nr_inelastic_modes,
-            nr_components=nr_strain_components,
-            Ue=Ue,
-            svd_algorithm=svd_algorithm,
+            i_arrays, nr_inelastic_modes, Ue=Ue, svd_algorithm=svd_algorithm
         )
         os.rename("singular_values.dat", "singular_values_inelastic.dat")
-        U = np.hstack([Ue, Ui])
+        U = numpy.hstack([Ue, Ui])
+
+    # Needs modificacion of e_files + i_files to work
     else:
         logger.info(
             "Nr of elastic modes set to zero -> Not discriminating elastic/inelastic snapshots"
         )
-        U = compute_modes(
-            nr_ips,
-            e_files + i_files,
-            nr_inelastic_modes,
-            nr_components=nr_strain_components,
-            svd_algorithm=svd_algorithm,
-        )
+        U = compute_modes(i_arrays, nr_inelastic_modes, svd_algorithm=svd_algorithm)
     t1 = time.time()
-    np.save(bases_fname, U)
+    numpy.save(bases_fname, U)
     logger.info("  Writing time: {:.1f}s".format(time.time() - t1))
     logger.info("  Total time: {:.1f}s".format(time.time() - t0))
     logger.info("")
