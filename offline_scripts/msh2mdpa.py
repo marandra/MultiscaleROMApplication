b88801ebecc3e4cab8cef195148f96558ea2f176
Marcelo <marcelo.raschi@gmail.com>
2020-02-26 16:42:49 +0100

    Adding more element types to conversion script (WIP)

--- offline_scripts/msh2mdpa.py
+++ offline_scripts/msh2mdpa.py
@@ -3,6 +3,62 @@ import sys
 import numpy
 
 
+def write_points(fo, points):
+    fo.write("Begin Nodes\n")
+    for i, p in enumerate(points):
+        fo.write("{:6d}   {:6f} {:6f} {:6f}\n".format(i + 1, p[0], p[1], p[2]))
+    fo.write("End Nodes\n")
+    fo.write("\n")
+
+
+def write_hexahedron(fo, elems):
+    fo.write("Begin Elements SmallDisplacementElement3D8N\n")
+    for i, p in enumerate(elems):
+        fo.write(
+            "{:6d}  0  {:6d} {:6d} {:6d} {:6d} {:6d} {:6d} {:6d} {:6d}\n".format(
+                i + 1, p[0], p[1], p[2], p[3], p[4], p[5], p[6], p[7]
+            )
+        )
+    fo.write("End Elements\n")
+    fo.write("\n")
+    return i + 1
+
+
+def write_wedge(fo, elems):
+    fo.write("Begin Elements SmallDisplacement{}3D6N\n")
+    for i, p in enumerate(elems):
+        fo.write(
+            "{:6d}  0  {:6d} {:6d} {:6d} {:6d} {:6d} {:6d}\n".format(
+                i + 1, p[0], p[1], p[2], p[3], p[4], p[5]
+            )
+        )
+    fo.write("End Elements\n")
+    fo.write("\n")
+    return i + 1
+
+
+def write_quad(fo, elems):
+    fo.write("Begin Conditions SurfaceCondition3D4N\n")
+    for i, r in enumerate(elems):
+        fo.write(
+            "{:6d}  0  {:6d} {:6d} {:6d} {:6d}\n".format(i + 1, r[0], r[1], r[2], r[3])
+        )
+    fo.write("End Conditions\n\n")
+    return i + 1
+
+def write_traingle(fo, elems):
+    fo.write("Begin Conditions SurfaceCondition3D3N\n")
+    for i, r in enumerate(elems):
+       fo.write(
+           "{:6d}  0  {:6d} {:6d} {:6d}\n".format(max_i + i + 1, r[0], r[1], r[2])
+       )
+    fo.write("End Conditions\n\n")
+    return i + 1
+
+######################################################
+# main
+######################################################
+
 i_filename = sys.argv[1]
 o_filename = sys.argv[2]
 mesh = meshio.read(i_filename)
@@ -12,6 +68,7 @@ print("")
 print("*********** WIP notice *************")
 print("Elements currently implemented:")
 print("   hexahedron")
+print("   wedge")
 print("   quad")
 print("************************************")
 with open(o_filename, "w") as fo:
@@ -23,39 +80,16 @@ with open(o_filename, "w") as fo:
     fo.write("End Properties\n")
     fo.write("\n")
 
-    fo.write("Begin Nodes\n")
-    # mdpa nodes indexes can be zero, so we wont need to convert meshio internal numeration
-    for i, p in enumerate(mesh.points):
-        fo.write("{:6d}   {:6f} {:6f} {:6f}\n".format(i, p[0], p[1], p[2]))
-    fo.write("End Nodes\n")
-    fo.write("\n")
+    write_points(fo, mesh.points)
+    max_i = 0
+    for cb in mesh.cells:
+        if "hexa" in cb[0]:
+            max_i += write_hexahedron(fo, cb[1])
+        elif "wedge" in cb[0]:
+            max_i += write_wedge(fo, cb[1])
+        elif "quad" in cb[0]:
+            max_i += write_quad(fo, cb[1])
 
-    fo.write("Begin Elements SmallDisplacementElement3D8N\n")
-    # mdpa elemens indexes can not be zero
-    for i, p in enumerate(mesh.cells["hexahedron"]):
-        fo.write(
-            "{:6d}  0  {:6d} {:6d} {:6d} {:6d} {:6d} {:6d} {:6d} {:6d}\n".format(
-                i + 1, p[0], p[1], p[2], p[3], p[4], p[5], p[6], p[7]
-            )
-        )
-    fo.write("End Elements\n")
-    fo.write("\n")
-
-    fo.write("Begin Conditions SurfaceCondition3D4N\n")
-    for i, r in enumerate(mesh.cells["quad"]):
-        fo.write(
-            "{:6d}  0  {:6d} {:6d} {:6d} {:6d}\n".format(i + 1, r[0], r[1], r[2], r[3])
-        )
-    fo.write("End Conditions\n\n")
-    max_i = i + 1
-
-    # fo.write("Begin Conditions SurfaceCondition3D3N\n")
-    # for i, r in enumerate(mesh.cells["triangle"]):
-    #    fo.write(
-    #        "{:6d}  0  {:6d} {:6d} {:6d}\n".format(max_i + i + 1, r[0], r[1], r[2])
-    #    )
-    # fo.write("End Conditions\n\n")
-    # max_i = i
 
     fo.write("Begin SubModelPart SKIN\n")
     fo.write("    Begin SubModelPartNodes\n")
