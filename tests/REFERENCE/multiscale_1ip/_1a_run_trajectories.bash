9ab8076177ee5739fe73e682451db8276310aeea
Marcelo Raschi <mraschi@cimne.upc.edu>
2020-03-02 18:03:01 +0100

    Update reference case files

--- tests/REFERENCE/multiscale_1ip/_1a_run_trajectories.bash
+++ tests/REFERENCE/multiscale_1ip/_1a_run_trajectories.bash
@@ -2,18 +2,18 @@
 
 for TRJ in case_*
 do
-        echo "Launching job "$TRJ
-        echo "export OMP_NUM_THREADS=1" > tmp_$TRJ.bash
-        echo "export PYTHONPATH=${PYTHONPATH}" >> tmp_$TRJ.bash
-        echo "export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}" >> tmp_$TRJ.bash
-        echo "cd "$TRJ >> tmp_$TRJ.bash
-	echo "time python3 MainKratos.py > outMainKratos 2>&1" >> tmp_$TRJ.bash
-        echo "cd .." >> tmp_$TRJ.bash
-        echo "rm tmp_$TRJ.bash" >> tmp_$TRJ.bash
+  echo "Launching job "$TRJ
+  echo "export OMP_NUM_THREADS=1" > tmp_$TRJ.bash
+  echo "export PYTHONPATH=${PYTHONPATH}" >> tmp_$TRJ.bash
+  echo "export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}" >> tmp_$TRJ.bash
+  echo "cd "$TRJ >> tmp_$TRJ.bash
+  echo "/usr/bin/time -v -o time.dat python MainKratos.py > outMainKratos" >> tmp_$TRJ.bash
+  echo "cd .." >> tmp_$TRJ.bash
+  echo "rm tmp_$TRJ.bash" >> tmp_$TRJ.bash
 done
 
 for TRJ in case_*
 do
-        bash tmp_$TRJ.bash
-        sleep 0.05
+  bash tmp_$TRJ.bash
+  sleep 0.05
 done
