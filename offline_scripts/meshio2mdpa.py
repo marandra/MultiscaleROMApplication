afe1f31f62c0e05795cf21c60bb55f31978b6bcd
marandra <marcelo.raschi@gmail.com>
2020-11-13 13:54:03 +0100

    Add main function

--- offline_scripts/meshio2mdpa.py
+++ offline_scripts/meshio2mdpa.py
@@ -96,104 +96,112 @@ def write_submodelpart(fo, group_name, points=[], cells=[], conditions=[]):
 ######################################################
 # main
 ######################################################
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        exit(
+            f"usage {sys.argv[0]} file_i file_o.mdpa\n"
+            "where file_i is any meshio accepted file"
+        )
+    i_filename = sys.argv[1]
+    o_filename = sys.argv[2]
+    mesh = meshio.read(i_filename)
+    print(mesh)
+    print("")
 
-i_filename = sys.argv[1]
-o_filename = sys.argv[2]
-mesh = meshio.read(i_filename)
-print(mesh)
-print("")
+    print("*********** WIP notice *************")
+    print("Elements currently implemented:")
+    print("   hexahedron")
+    print("   wedge")
+    print("Conditions currently implemented:")
+    print("   quad")
+    print("   triangle")
+    print("************************************")
 
-print("*********** WIP notice *************")
-print("Elements currently implemented:")
-print("   hexahedron")
-print("   wedge")
-print("Conditions currently implemented:")
-print("   quad")
-print("   triangle")
-print("************************************")
-with open(o_filename, "w") as fo:
+    with open(o_filename, "w") as fo:
 
-    #  Header
-    write_header(fo)
+        #  Header
+        write_header(fo)
 
-    #  Nodes
-    write_points(fo, mesh.points)
+        #  Nodes
+        write_points(fo, mesh.points)
 
-    #  Elements
-    element_offset = []
-    offset = 0
-    for cell_block in mesh.cells:
-        element_type = cell_block[0]
-        element_data = cell_block[1]
-        if "hexa" in element_type:
-            element_offset.append(offset)
-            offset += write_elements_hexahedron(fo, element_data, offset=offset)
-        elif "wedge" in element_type:
-            element_offset.append(offset)
-            offset += write_elements_wedge(fo, element_data, offset=offset)
-    nr_cells = offset
+        #  Elements
+        element_offset = []
+        offset = 0
+        for cell_block in mesh.cells:
+            element_type = cell_block[0]
+            element_data = cell_block[1]
+            if "hexa" in element_type:
+                element_offset.append(offset)
+                offset += write_elements_hexahedron(fo, element_data, offset=offset)
+            elif "wedge" in element_type:
+                element_offset.append(offset)
+                offset += write_elements_wedge(fo, element_data, offset=offset)
+        nr_cells = offset
 
-    # Conditions. Surface elements obtained exploiding volume elements
-    # my script: generating skin
-    quads = []
-    triangles = []
-    for cell_block in mesh.cells:
-        element_type = cell_block[0]
-        element_data = cell_block[1]
-        if "hexa" in element_type:
-            for e in element_data:
-                quads.extend(skin_detect.explode_hexa(e))
-        elif "wedge" in element_type:
-            for e in element_data:
-                qs, ts = skin_detect.explode_prism(e)
-                quads.extend(qs)
-                triangles.extend(ts)
-    skin_quads = skin_detect.filter_faces(quads)
-    skin_triangles = skin_detect.filter_faces(triangles)
-    cb_q = meshio.CellBlock("quads", numpy.array(skin_quads))
-    cb_t = meshio.CellBlock("triangles", numpy.array(skin_triangles))
-    skin_cells = [cb_q, cb_t]
-    # end of my scrip
+        # Conditions. Surface elements obtained exploiding volume elements
+        # my script: generating skin
+        quads = []
+        triangles = []
+        for cell_block in mesh.cells:
+            element_type = cell_block[0]
+            element_data = cell_block[1]
+            if "hexa" in element_type:
+                for e in element_data:
+                    quads.extend(skin_detect.explode_hexa(e))
+            elif "wedge" in element_type:
+                for e in element_data:
+                    qs, ts = skin_detect.explode_prism(e)
+                    quads.extend(qs)
+                    triangles.extend(ts)
+        skin_quads = skin_detect.filter_faces(quads)
+        skin_triangles = skin_detect.filter_faces(triangles)
+        cb_q = meshio.CellBlock("quads", numpy.array(skin_quads))
+        cb_t = meshio.CellBlock("triangles", numpy.array(skin_triangles))
+        skin_cells = [cb_q, cb_t]
+        # end of my scrip
 
-    condition_offset = []
-    offset = 0
-    for cell_block in skin_cells:
-        condition_type = cell_block[0]
-        condition_data = cell_block[1]
-        if "quad" in condition_type:
-            condition_offset.append(offset)
-            offset += write_conditions_quad(fo, condition_data, offset=offset)
-        elif "triangle" in condition_type:
-            condition_offset.append(offset)
-            offset += write_conditions_triangle(fo, condition_data, offset=offset)
-    nr_conditions = offset
+        condition_offset = []
+        offset = 0
+        for cell_block in skin_cells:
+            condition_type = cell_block[0]
+            condition_data = cell_block[1]
+            if "quad" in condition_type:
+                condition_offset.append(offset)
+                offset += write_conditions_quad(fo, condition_data, offset=offset)
+            elif "triangle" in condition_type:
+                condition_offset.append(offset)
+                offset += write_conditions_triangle(fo, condition_data, offset=offset)
+        nr_conditions = offset
 
-    # Groups (as submodelparts)
-    for group_name, cell_arrays in mesh.cell_sets.items():
-        group_cells = []
-        for i, e in enumerate(cell_arrays):
-            if len(e) == 0:
-                continue
-            g = element_offset[i]
-            elements = e + g
-            group_cells.extend(elements)
-        write_submodelpart(fo, group_name, cells=group_cells)
+#        # Groups (as submodelparts)
+#        for group_name, cell_arrays in mesh.cell_sets_dict.items():
+#            group_cells = []
+#            for i, e in enumerate(cell_arrays):
+#                if len(e) == 0:
+#                    continue
+#                g = element_offset[i]
+#                elements = e + g
+#                group_cells.extend(elements)
+#            write_submodelpart(fo, group_name, cells=group_cells)
 
-    # Custom groups
-    # TODO: add following submodelparts as groups in mesh.cell_sets,
-    # so we avoid these write_submodelparts()
-    p_origin = numpy.where(
-        (mesh.points[:, 0] == 0) * (mesh.points[:, 1] == 0) * (mesh.points[:, 2] == 0)
-    )[0]
-    write_submodelpart(fo, "PINNED", p_origin)
-    points = [x for x in range(len(mesh.points))]
-    cells = [x for x in range(nr_cells)]
-    write_submodelpart(fo, "RVE", points=points, cells=cells)
-    aux_points = []
-    for cb in skin_cells:
-        for a in cb[1]:
-            aux_points.extend(a)
-    skin_points = set(aux_points)
-    points = [x for x in skin_points]
-    conditions = [x for x in range(nr_conditions)]
-    write_submodelpart(fo, "SKIN", points=points, conditions=conditions)
+        # Custom groups
+        # TODO: add following submodelparts as groups in mesh.cell_sets,
+        # so we avoid these write_submodelparts()
+        p_origin = numpy.where(
+            (mesh.points[:, 0] == 0)
+            * (mesh.points[:, 1] == 0)
+            * (mesh.points[:, 2] == 0)
+        )[0]
+        write_submodelpart(fo, "PINNED", p_origin)
+        points = [x for x in range(len(mesh.points))]
+        cells = [x for x in range(nr_cells)]
+        write_submodelpart(fo, "RVE", points=points, cells=cells)
+        aux_points = []
+        for cb in skin_cells:
+            for a in cb[1]:
+                aux_points.extend(a)
+        skin_points = set(aux_points)
+        points = [x for x in skin_points]
+        conditions = [x for x in range(nr_conditions)]
+        write_submodelpart(fo, "SKIN", points=points, conditions=conditions)
